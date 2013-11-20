332570c57844dbee931fa8d696a258a637db0a0b
Matthias Koenig <matthias.koenig@charite.de>
2013-11-20 19:27:33 +0100

    Testing of polls implemented

--- python/mysite/polls/views.py
+++ python/mysite/polls/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse, Http404, HttpResponseRedirect
 from django.template import RequestContext, loader
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 
@@ -12,12 +13,23 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_poll_list'
 
     def get_queryset(self):
-        """Return the last five published polls."""
-        return Poll.objects.order_by('-pub_date')[:5]
+        """
+        Return the last five published polls (not including those set to be
+        published in the future).
+        """
+        return Poll.objects.filter(
+                pub_date__lte=timezone.now()
+          ).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
     model = Poll
     template_name = 'polls/detail.html'
+    
+    def get_queryset(self):
+        """
+        Excludes any polls that aren't published yet.
+        """
+        return Poll.objects.filter(pub_date__lte=timezone.now())
 
 
 class ResultsView(generic.DetailView):
