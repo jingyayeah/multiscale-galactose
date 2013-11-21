70c9dc2b9c2c93945ecd6ad663421c92ec682965
Matthias Koenig <matthias.koenig@charite.de>
2013-11-21 13:03:24 +0100

    Example simulation added

---
+++ python/SinusoidSimulator/core/RunSimulation.py
@@ -0,0 +1,21 @@
+'''
+Created on Nov 21, 2013
+
+@author: mkoenig
+
+Simulations in Cpp have to be called with
+SBML file and the parameter settings files
+
+d
+
+
+'''
+
+
+
+if __name__ == "__main__":
+    
+    
+    
+    
+    print "Hello world"
