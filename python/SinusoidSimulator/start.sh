647f9e9956d88e0e2314856a764d9e4a73a48149
Matthias Koenig <matthias.koenig@charite.de>
2013-11-26 15:59:15 +0100

    bash start script

---
+++ python/SinusoidSimulator/start.sh
@@ -0,0 +1,8 @@
+# python path settings for django
+export PYTHONPATH=$PYTHONPATH:/home/mkoenig/multiscale-galactose/python
+export PYTHONPATH=$PYTHONPATH:/home/mkoenig/multiscale-galactose/python/mysite/
+
+export DJANGO_SETTINGS_MODULE=mysite.settings
+
+cd core
+python CoreOverview.py
