daf858faa5a1fbf416d5387e58774856feb79899
Matthias Koenig <konigmatt@googlemail.com>
2014-03-26 13:02:46 +0100

    Copying Copasi libraries for project to build against

--- startSimulation.sh
+++ startSimulation.sh
@@ -30,6 +30,13 @@ mkdir ~/multiscale-galactose-results/tmp_sim
 echo "###################################"
 echo "# build copasi #"
 echo "###################################"
+# copy all the libraries to link
+COPASI_PROJECT=~/multiscale-galactose/cpp/copasi/CopasiModelRunner
+cp ~/copasi/build_copasi/copasi/libCOPASISE.a $COPASI_PROJECT
+cp ~/copasi/copasi-dependencies/bin/lib/*.a $COPASI_PROJECT
+cp ~/copasi/copasi-dependencies/bin/lib/*.la $COPASI_PROJECT
+
+
 rm -r ~/multiscale-galactose/cpp/copasi/CopasiModelRunner/Debug
 rm -r ~/multiscale-galactose/cpp/copasi/CopasiModelRunner/build
 mkdir ~/multiscale-galactose/cpp/copasi/CopasiModelRunner/build
@@ -40,7 +47,7 @@ make
 echo "###################################"
 echo "# run simulations #"
 echo "###################################"
-cd ~/multiscale-galactose/python/SinusoidSimulator/core
-python Simulator.py
+# cd ~/multiscale-galactose/python/SinusoidSimulator/core
+# python Simulator.py
 
 
