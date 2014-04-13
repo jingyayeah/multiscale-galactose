d4515efbeb81a290416f99c2ff9659f3a1f2b206
Matthias Koenig <konigmatt@googlemail.com>
2014-04-13 09:08:11 +0200

    Starting parameter distribution scripts R

---
+++ R/ParameterDistributions.R
@@ -0,0 +1,28 @@
+rm(list=ls())
+
+################################################################
+## Parameter Distributions ##
+################################################################
+# author: Matthias Koenig
+# date: 2014-04-13
+################################################################
+results.folder <- "/home/mkoenig/multiscale-galactose-results"
+code.folder    <- "/home/mkoenig/multiscale-galactose/R" 
+data.folder    <- "/home/mkoenig/multiscale-galactose/experimental_data/parameter_distributions/"
+
+setwd(results.folder)
+
+###############################################################
+# Load experimental data
+###############################################################
+
+
+###############################################################
+# Load the simulated parameter distribution
+###############################################################
+
+
+
+###############################################################
+# Plot experimental data with simulations
+###############################################################
