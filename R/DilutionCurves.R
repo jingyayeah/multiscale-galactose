6caa7b7b3e4f69f2e84c4434accbaea09e338080
Matthias Koenig <matthias.koenig@charite.de>
2014-04-20 19:57:06 +0200

    Mean MultipleIndicator curves plotted

--- R/DilutionCurves.R
+++ R/DilutionCurves.R
@@ -8,152 +8,97 @@
 # date: 2014-04-20
 ################################################################
 rm(list=ls())
-library(MultiscaleAnalysis)
+library('MultiscaleAnalysis')
+library('matrixStats')
 setwd(ma.settings$dir.results)
-
-sname <- '2014-04-20_MultipleIndicator'
-ma.settings$dir.simdata <- file.path(ma.settings$dir.results, sname, 'data')
-tasks <- c('T1', 'T2', 'T3', 'T4', 'T5')
-peaks <- c('P00', 'P01', 'P02', 'P03', 'P04')
-
-  task <- tasks[1]
-  peak <- peaks[1]
-  modelId <- paste('MultipleIndicator_', peak, '_v10_Nc20_Nf1', sep='')
-  parsfile <- file.path(ma.settings$dir.results, sname, 
-                        paste(task, '_', modelId, '_parameters.csv', sep=""))
-  load(file=outfileFromParsfile(parsfile))
-
-summary(pars)
 
 compounds = c('gal', 'rbcM', 'alb', 'suc', 'h2oM')
 ccolors = c('black', 'red', 'darkgreen', 'darkorange', 'darkblue')
 pv_compounds = paste('PV__', compounds, sep='')
 names(ccolors) <- pv_compounds
 
-####################################################################
-
-library('matrixStats')
-
-# Plot the single curves with the mean
-plotCompound <- function(time, data, name, col="black", ylim=c(0.0, 0.2), xlim=c(0, 30)){
-  # plot the single curves
-  plot(numeric(0), numeric(0), 'l', main=name,
-       xlab="time [s]", ylab="c [mM]", ylim=ylim, xlim=xlim)
+# Plot all the single curves with mean and std
+createAllPlot <- function(create_plot_files=F){
+  time <- readTimeForSimulation(ma.settings$dir.simdata, rownames(pars)[1])-10.0
+  Nc <- length(pv_compounds)
   
-  Nsim <- ncol(data)
-  for (ks in seq(Nsim)){
-      lines(time, data[,ks], col="gray")
+  # Create the plot
+  if (create_plot_files == TRUE){
+    png(filename=paste(ma.settings$dir.results, '/', task, "_Dilution_Curves.png", sep=""),
+        width = 4000, height = 1000, units = "px", bg = "white",  res = 200)
   }
-  
-  # plot the mean and variance for time courses
-  # TODO how to better calculate -> what error measurment to use
-  rmean <- rowMeans(data)
-  rstd <- rowSds(data)
-  lines(time, rmean, col=col, lwd=2)
-  lines(time, rmean+rstd, col=col, lwd=2, lty=2)
-  lines(time, rmean-rstd, col=col, lwd=2, lty=2)
-}
-
-# Make a scatter plot
-plotCompoundScatter <- function(time, data, name, col="black", ylim=c(0.0, 0.2), xlim=c(0, 30)){
-  # plot the single curves
-  plot(numeric(0), numeric(0), 'l', main=name,
-       xlab="time [s]", ylab="c [mM]", ylim=ylim, xlim=xlim)
-  
-  Nsim <- ncol(data)
-  for (ks in seq(Nsim)){
-    points(time,data[,ks], col=rgb(0,100,0,25,maxColorValue=255), pch=16)
-    #lines(time, data[,ks], col="gray")
+  par(mfrow=c(1,Nc))
+  for (name in pv_compounds){
+    plotCompound(time, MI.mat[[name]], name, col=ccolors[name], xlim=c(0,15), ylim=c(0,2.5))
   }
-  
-  # plot the mean and variance for time courses
-  # TODO how to better calculate -> what error measurment to use
-  rmean <- rowMeans(data)
-  rstd <- rowSds(data)
-  lines(time, rmean, col=col, lwd=2)
-  lines(time, rmean+rstd, col=col, lwd=2, lty=2)
-  lines(time, rmean-rstd, col=col, lwd=2, lty=2)
-}
-
-plot2Ddensity <- function(time, data, name, col="black", ylim=c(0.0, 0.2), xlim=c(0, 30)){
-  
-  library('KernSmooth')
-  # prepare data
-  Nsim <- ncol(data)
-  tmax_ind <- length(which(time<100))
-  Nt <- length(time[1:tmax_ind])
-  x <- matrix(NA, nrow=Nsim*Nt, ncol=2)
-  for (ks in seq(Nsim)){
-    indices <- ((ks-1)*Nt+1):(ks*Nt)  
-    tmp <- cbind(time[1:tmax_ind], data[1:tmax_ind, ks])
-    x[indices,] <- tmp
+  par(mfrow=c(1,1))
+  if (create_plot_files == TRUE){
+    dev.off()
   }
-  est <- bkde2D(x, bandwidth=c(2, 0.02), gridsize=c(100,200), range.x=list(c(0,100), c(0,0.5)) )
-  contour(est$x1, est$x2, est$fhat)
-  # plot the mean and variance for time courses
-  # TODO how to better calculate -> what error measurment to use
-  for (ks in seq(Nsim)){
-    points(time,data[,ks], col=rgb(0,100,0,20,maxColorValue=255), pch=16)
-    #lines(time, data[,ks], col="gray")
+}
+
+## Combined Dilution Curves in one plot ##
+createMeanPlot <- function(create_plot_files=F){
+  time <- readTimeForSimulation(ma.settings$dir.simdata, rownames(pars)[1])-10.0
+  Nc <- length(pv_compounds)
+  
+  if (create_plot_files == TRUE){
+    png(filename=paste(ma.settings$dir.results, '/', task, "_Dilution_Curves_Combined.png", sep=""),
+        width = 1000, height = 1000, units = "px", bg = "white")
   }
-  rmean <- rowMeans(data)
-  rstd <- rowSds(data)
-  lines(time, rmean, col=col, lwd=2)
-  lines(time, rmean+rstd, col=col, lwd=2, lty=2)
-  lines(time, rmean-rstd, col=col, lwd=2, lty=2)
-
-}
-name="PV__alb"
-plot2Ddensity(time, MI.mat[[name]][,], name, col=ccolors[name], ylim=c(0,0.8))
-
-time <- readTimeForSimulation(ma.settings$dir.simdata, rownames(pars)[1]) -10.0
-Nc <- length(pv_compounds)
-
-create_plot_files = FALSE
-if (create_plot_files == TRUE){
-  png(filename=paste(ma.settings$dir.results, '/', task, "_Dilution_Curves.png", sep=""),
-    width = 4000, height = 1000, units = "px", bg = "white",  res = 200)
-}
-par(mfrow=c(1,Nc))
-for (name in pv_compounds){
-  plotCompound(time, MI.mat[[name]], name, col=ccolors[name])
-}
-par(mfrow=c(1,1))
-if (create_plot_files == TRUE){
+  par(mfrow=c(1,1))
+  plot(numeric(0), numeric(0), 'l', 
+       xlab="time [s]", ylab="c [mM]", xlim=c(0,30), ylim=c(0,1.5))
+  for (kc in seq(1, length(compounds)) ){
+    for (name in pv_compounds){
+      plotCompoundMean(time, MI.mat[[name]], col=ccolors[name])
+    }
+  }
+  par(mfrow=c(1,1))
   dev.off()
 }
-dev.off()
 
-name="PV__alb"
-plotCompound(time, MI.mat[[name]][,1:200], name, col=ccolors[name], ylim=c(0,0.8))
+
+## Load the preprocessed data ##
+sname <- '2014-04-20_MultipleIndicator'
+version <- 'v11'
+ma.settings$dir.simdata <- file.path(ma.settings$dir.results, sname, 'data')
+tasks <- paste('T', seq(6,10), sep='')
+peaks <- c('P00', 'P01', 'P02', 'P03', 'P04')
+
+for (kt in seq(length(tasks))){
+  task <- tasks[kt]
+  peak <- peaks[kt]
+  modelId <- paste('MultipleIndicator_', peak, '_', version, '_Nc20_Nf1', sep='')
+  parsfile <- file.path(ma.settings$dir.results, sname, 
+                        paste(task, '_', modelId, '_parameters.csv', sep=""))
+  # Load the data
+  load(file=outfileFromParsFile(parsfile))
+  summary(pars)
+  
+  # Create the plots
+  # createAllPlot(create_plot_files=TRUE)
+  createMeanPlot(create_plot_files=TRUE)
+}
+
+####################################################################
+
+
+
+name="PV__rbcM"
+time <- readTimeForSimulation(ma.settings$dir.simdata, rownames(pars)[1])-10.0
+Nc <- length(pv_compounds)
+plotCompound(time, MI.mat[[name]], name, col=ccolors[name], ylim=c(0,0.8))
+
+boxplot(MI.mat[[name]][1:40, 1:100])
+plot2Ddensity(time, MI.mat[[name]][,], name, col=ccolors[name], ylim=c(0,0.8))
+
+
 plotCompoundScatter(time, MI.mat[[name]][,], name, col=ccolors[name], ylim=c(0,0.8))
 plot2Ddensity(time, MI.mat[[name]][,], name, col=ccolors[name], ylim=c(0,0.8))
 
-## Combined Dilution Curves in one plot ##
-png(filename=paste(info.folder, '/', task, "_Dilution_Curves_Combined.png", sep=""),
-    width = 1000, height = 1000, units = "px", bg = "white",  res = 150)
-par(mfrow=c(1,1))
-for (kc in seq(1, length(compounds)) ){
-  
-  # name = "PV__rbcM"
-  name = paste("PV__", compounds[kc], sep="")
-  # plot one compound
-  tmp <- dilmat[[name]]
-  # plot the mean and variance for time courses
-  rmean <- rowMeans(tmp)
-  rstd <- rowSds(tmp)
-  if (kc==1){
-    plot(time, rmean, col=ccolors[kc], lwd=3, 'l', 
-         main="Dilution Curves", xlab="time [s]", ylab="c [mM]", 
-         ylim=c(0.0, 0.20), xlim=c(0.0, 30) )
-  }else {
-    lines(time, rmean, col=ccolors[kc], lwd=3)
-  }
-  lines(time, rmean+rstd, col=ccolors[kc], lwd=1, lty=2)
-  lines(time, rmean-rstd, col=ccolors[kc], lwd=1, lty=2)
-}
-par(mfrow=c(1,1))
-dev.off()
+
+
 
 
 # calculate the maximum values
