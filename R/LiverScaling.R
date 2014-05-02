12d1201e8b3667e964ae0e1e4f6d664bd71fbde9
Matthias Koenig <konigmatt@googlemail.com>
2014-05-02 11:04:19 +0200

    scaling of values

--- R/LiverScaling.R
+++ R/LiverScaling.R
@@ -226,6 +226,8 @@ getProbabilitiesForSamples <- function(pars, p.gen, name){
 }
 
 # For every var_ps a distribution has to exist
+# !Make sure that over all the probabilities is integrated
+# !This 
 print(var_ps)
 Nsim <- nrow(pars)
 p_sample <- rep(1, Nsim)
@@ -248,22 +250,101 @@ plot(pars$y_cell, pars$p_y_cell)
 
 # weight samples with their probability i.e. get the probability for every
 # varied parameter and 
-calculateWeightedValue <- function(pars, name){
-    m <- sum(pars$p_sample * pars[[name]])
+
+# Calculate weighted values based on the probabilities for sample
+# Weighted mean, variance and standard deviation calculations
+
+# x and wt have to be of same length
+# wt.mean <- sum(wt*x)/sum(wt)
+wt.mean <- function(x, wt){
+ weighted.mean(x, wt)
 }
-m_L <- calculateWeightedValue(pars, 'L')
-m_L
-m_L2 <-  sum(pars$p_L * pars$L)
-m_L2
-head(pars)
+# unbiased variance, i.e. (N-1)
+wt.var <- function(x, wt){
+    y <- as.matrix(x)
+    c <- cov.wt(y, wt)
+    c$cov
+}
+# unbiased standard deviation
+wt.sd <- function(x, wt){
+    var <- wt.var(x, wt)
+    sqrt(var)
+}
+
+# Test the weighted values
+plotWeighted <- function (pars, p.gen, name) {
+  data = pars[[name]]
+  wt = pars$p_sample
+  # TODO: carful
+  # wt = pars[[paste('p_', name, sep="")]]
+  wmean <- wt.mean(data, wt)
+  wvar <- wt.var(data, wt)
+  wsd <- wt.sd(data, wt)
+  
+  # figure
+  cols = c('gray', 'black', 'red', 'blue')
+  lwd = rep(2,4)
+  type = rep('l',4)
+  cutoff = 0.9; lb = (1-cutoff)*min(data); ub = (1+cutoff)*max(data)
+  Np = 1000;
+  
+  # histogramm of samples
+  hist(data, freq=FALSE, breaks=20, xlim=c(lb, ub), 
+       main=paste(name, ' [', p.gen[name, 'unit'], ']', sep=""), 
+       col=cols[1])
+  
+  x <- seq(from=lb, to=ub, length.out=Np)
+  # Here also log normal distributions have to be used, so 
+  # transformation of mean and std has to be applied
+  # y <- dnorm(x, mean=mean, sd=sd)
+  # calculated distribution based on samples
+  mean <- mean(data)
+  sd <- sd(data)
+  meanlog = meanlog(mean, sd)
+  sdlog   = stdlog(mean, sd)
+  y <- dlnorm(x, meanlog=meanlog, sdlog=sdlog)
+  points(x, y, col=cols[2], type=type[2], lwd=lwd[2])
+  
+  # calculated distribution based on weighted samples with probabilies
+  #y <- dnorm(x, mean=wmean, sd=wsd)
+  wmeanlog = meanlog(wmean, wsd)
+  wsdlog   = stdlog(wmean, wsd)
+  y <- dlnorm(x, meanlog=wmeanlog, sdlog=wsdlog)
+  points(x, y, col=cols[3], type=type[3], lwd=lwd[3])
+  
+  # real distribution
+  scale_fac = p.gen[name, 'scale_fac'] 
+  xr <- seq(from=lb, to=ub*scale_fac, length.out=Np)
+  yr <- dlnorm(xr, meanlog=p.gen[name, 'meanlog'], sdlog=p.gen[name, 'sdlog'], log = FALSE)
+  xr <- xr/scale_fac
+  yr <- yr*scale_fac
+  points(xr, yr, col=cols[4], type=type[4], lwd=lwd[4])
+  
+  legend("topright", legend=c("samples", "samples dist", "weighted samples dist", "real dist"),
+         col=cols, lwd=lwd, cex=0.4)
+}
+
+var_ps
+name='flow_sin'
+plotWeighted(pars, p.gen, name)
+
+# Generate plots
+plot.width = 800 
+plot.height = 800
+plot.units= "px"
+plot.bg = "white"
+plot.res = 150
+for (name in var_ps){
+    fname = paste('test_distribution_', name, '.png', sep="") 
+    print(fname)
+    png(filename=fname, width=plot.width, height=plot.height, units=plot.units, bg=plot.bg, res=plot.res)
+    plotWeighted(pars, p.gen, name)
+    dev.off()
+}
+
+
 
 # calculate the combined probability based on the parameter probabilities
-name="y_cell"
-x <- seq(from=0, to=1000, length.out=1000)
-y <- dlnorm(x, meanlog=p.gen[name, 'meanlog'], sdlog=p.gen[name, 'sdlog'], log = FALSE)
-plot(x, y)
-help(dlnorm)
-plot(sort(pars$L), seq(1, length(pars$L)))
 
 
 
