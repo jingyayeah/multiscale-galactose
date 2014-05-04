4f3a8827741699627d3cd28169384d91a1a6195f
Matthias Koenig <matthias.koenig@charite.de>
2014-05-04 23:27:47 +0200

    Updated timecourse script

--- copyTimecourses.sh
+++ copyTimecourses.sh
@@ -5,6 +5,7 @@
 DATE=2014-05-04
 STYPE=MultipleIndicator
 IPS=(10.39.32.106 10.39.32.189 10.39.34.27)
+# IPS=(10.39.32.106)
 
 DATA_DIR=~/multiscale-galactose-results/${DATE}_${STYPE}
 mkdir $DATA_DIR 
