e188fc602525a2b9432b8ff216fc0e3125675699
Matthias Koenig <konigmatt@googlemail.com>
2014-05-07 10:01:18 +0200

    Woking on the python SBML report

--- python/mysite/sim/report/ReportFactory.py
+++ python/mysite/sim/report/ReportFactory.py
@@ -5,34 +5,70 @@ The original prototype was developed in Java with JSBML based on a
 different template language but the underlying idea of rendering the
 template with the given SBML structure remains the same.
 
-But some other dependencies.
-TODO: implement
 
 @author: Matthias Koenig
 @date: 2014-05-07
 '''
 
+import os
+import sys
+sys.path.append('/home/mkoenig/multiscale-galactose/python')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
+from django.http.response import HttpResponse
+from django.template import loader, Context
 import libsbml
-# TODO: fix the pythonpath
 
-def createSBMLReport():
+
+def createSBMLReport(sbml_file, report_file):
     '''
     Creates the SBML report by rendering a Django view.
     '''
+    # TODO: convert the full java template to python
+    # TODO: update the report CSS
+    # TODO: create a proper database view
+    
     
     # Read the sbml data structure
+    print sbml_file
+    doc = libsbml.readSBMLFromFile(sbml_file)
+    model = doc.getModel()
+    print model.getId()
     
-    cores_list = Core.objects.order_by("-time")
-    template = loader.get_template('sim/cores.html')
-    context = RequestContext(request, {
-        'cores_list': cores_list,
+    lofFD = model.getListOfFunctionDefinitions()
+    print lofFD
+    print lofFD.size()
+    
+    # TODO: pack the Django model in additon to provide full access
+    #        to the database information
+    
+    # All python objects can be packed in the HTML context
+    template = loader.get_template('report/SBMLReport.html')
+    context = Context({
+        'model': model,
+        'unitDefinitions' : model.getListOfUnitDefinitions(),
+        'compartments' : model.getListOfCompartments(),
+        'functions' : model.getListOfFunctionDefinitions(),
+        'parameters' : model.getListOfParameters(),
+        'rules' : model.getListOfRules(),
+        'assignments' : model.getListOfInitialAssignments(),
+        'species' : model.getListOfSpecies(),
+        'reactions' : model.getListOfReactions(),
+        'constraints' : model.getListOfConstraints(),
+        'events' : model.getListOfEvents(),
     })
-    return HttpResponse(template.render(context))
-
-    print 'Not Implemented!'
-    pass
+    # Render the html template with the sbml context
+    rtemp = template.render(context)
+    
+    # Write the HTML report
+    f = open(report_file, "w")
+    f.write(rtemp)
+    
+    
 
 
 if __name__ == "__main__":
-    createSBMLReport()
+    sbml_file = "/home/mkoenig/multiscale-galactose-results/tmp_sbml/Galactose_v14_Nc1_Nf1.xml"
+    report_file = "/home/mkoenig/tmp/test.html"
+    createSBMLReport(sbml_file, report_file)
+    print "DONE"
