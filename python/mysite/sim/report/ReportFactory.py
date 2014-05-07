b9f39d5b320d261af6f2b33129edeb3e5e1ef6fa
Matthias Koenig <konigmatt@googlemail.com>
2014-05-07 13:49:09 +0200

    Updated report

--- python/mysite/sim/report/ReportFactory.py
+++ python/mysite/sim/report/ReportFactory.py
@@ -11,7 +11,7 @@ template with the given SBML structure remains the same.
 
 import os
 import sys
-from libsbml import SBMLDocument
+from libsbml import SBMLDocument, ListOfUnitDefinitions, ListOf, UnitDefinition
 sys.path.append('/home/mkoenig/multiscale-galactose/python')
 os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
 
@@ -28,6 +28,10 @@ def report(request, model_pk):
         TODO: convert the full java template to python
         TODO: update the report CSS
         TODO: backup solution to query via the id vs. pk
+        TODO: how to call functions from within the Django template
+        
+        TODO: write some additional methods for the SBase to display the annotations.
+        
     '''
     # Get the database model
     sbmlmodel = get_object_or_404(SBMLModel, pk=model_pk)
@@ -40,22 +44,37 @@ def report(request, model_pk):
     # doc = libsbml.readSBMLFromFile(sbml_file)
     doc = libsbml.readSBMLFromFile(str(sbml_path))
     model = doc.getModel()
-
+    
     # Render the template with the data
     template = loader.get_template('report/SBMLReport.html')
     context = RequestContext(request, {
         'sbmlmodel' : sbmlmodel,
         'model': model,
-        'unitDefinitions' : model.getListOfUnitDefinitions(),
+        'units' : model.getListOfUnitDefinitions(),
+        'units_size' : model.getListOfUnitDefinitions().size(),
         'compartments' : model.getListOfCompartments(),
+        'compartments_size' : model.getListOfCompartments().size(),
         'functions' : model.getListOfFunctionDefinitions(),
+        'functions_size' : model.getListOfFunctionDefinitions().size(),
         'parameters' : model.getListOfParameters(),
+        'parameters_size' : model.getListOfParameters().size(),
         'rules' : model.getListOfRules(),
+        'rules_size' : model.getListOfRules().size(),
         'assignments' : model.getListOfInitialAssignments(),
+        'assignments_size' : model.getListOfInitialAssignments().size(),
         'species' : model.getListOfSpecies(),
+        'species_size' : model.getListOfSpecies().size,
         'reactions' : model.getListOfReactions(),
+        'reactions_size' : model.getListOfReactions().size(),
         'constraints' : model.getListOfConstraints(),
+        'constraints_size' : model.getListOfConstraints().size(),
         'events' : model.getListOfEvents(),
+        'events_size' : model.getListOfEvents().size(),
     })
     return HttpResponse(template.render(context))
 
+def test():
+    ListOfUnitDefinitions.size()
+    
+    
+    
