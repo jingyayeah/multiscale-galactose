b1c5a70964a869cec5b3a3259afdc7b5a5cb832d
Matthias Koenig <matthias.koenig@charite.de>
2014-05-13 09:48:37 +0200

    Preprocessing the MI results

--- R/MultipleIndicatorPreprocessing.R
+++ R/MultipleIndicatorPreprocessing.R
@@ -1,14 +1,14 @@
 ################################################################
 ## Preprocess the MultipleIndicatorDilution Data ##
 ################################################################
-# Read the timecourse data and create optimized data structures
-# for query.
-# Change the necessary settings for the respective SBML networks,
-# parameter files.
+# Read the timecourse data and creates reduced data structures
+# for simplified query and visualization.
+# Necessary to select the column indices (i.e. model components)
+# which should be part of the reduced model structure.
 # Run from terminal via -> Rscript MultipleIndicatorPreprocessing.R
 #
 # author: Matthias Koenig
-# date: 2014-04-29
+# date: 2014-05-13
 # install.packages('data.table')
 
 rm(list=ls())   # Clear all objects
@@ -16,21 +16,21 @@ library(data.table)
 library(MultiscaleAnalysis)
 setwd(ma.settings$dir.results)
 
-sname <- '2014-05-07_MultipleIndicator'
+sname <- '2014-05-13_MultipleIndicator'
+version <- 'v17'
 ma.settings$dir.simdata <- file.path(ma.settings$dir.results, sname, 'data')
-tasks <- paste('T', seq(1,5), sep='')
-peaks <- c('P00', 'P01', 'P02', 'P03', 'P04')
+task.offset <- 27
+task.seq <- seq(0,2)
+tasks <- paste('T', task.offset+task.seq, sep='')
+peaks <- paste('P0', task.seq, sep='')
 
 for (kt in seq(length(tasks))){
   task <- tasks[kt]
   peak <- peaks[kt]
-  modelId <- paste('MultipleIndicator_', peak, '_v14_Nc20_Nf1', sep='')
+  modelId <- paste('MultipleIndicator_', peak, '_', version, '_Nc20_Nf1', sep='')
   parsfile <- file.path(ma.settings$dir.results, sname, 
                         paste(task, '_', modelId, '_parameters.csv', sep=""))
 
-  # Do the preprocessing
-  # preprocess(parsfile=parsfile, sim.dir=ma.settings$dir.simdata, 
-  #            outFile='test.out', max_index=2)
-  preprocess(parsfile, ma.settings$dir.simdata)
+  # preprocessing
+  preprocessPPPV(parsfile, ma.settings$dir.simdata)
 }
-rm(peak, task, kt)
