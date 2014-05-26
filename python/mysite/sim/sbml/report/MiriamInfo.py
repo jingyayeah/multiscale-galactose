cf2763e148f6822d476aa690d8a35bab3855d576
Matthias Koenig <konigmatt@googlemail.com>
2014-05-26 17:47:07 +0200

    Work on MIRIAM RestFul interface

--- python/mysite/sim/sbml/report/MiriamInfo.py
+++ python/mysite/sim/sbml/report/MiriamInfo.py
@@ -9,6 +9,11 @@ sudo apt-get install python-simplejson
 https://pypi.python.org/pypi/siesta
 
 
+    /datatypes/: retrieves the list of all available datatypes
+    /datatypes/$id: retrieves the datatype identified by $id (for example: MIR:00000008)
+    /resolve/$urn: resolves the given MIRIAM URN (for example: urn:miriam:uniprot:P62158)
+    /version: displays the current version of the services
+
 
 api = API('http://myrestful/api/v1')
       
@@ -28,6 +33,14 @@ api.resoure(id).resource_collection().get()
 
 
 from siesta import API
+
+
+api = API('http://www.ebi.ac.uk/miriam/main/export/')
+res, response = api.resource.get(format='json')
+print res.get()
+print response
+
+
 
 
 
