19649482f86b93eceb868a9e68a133202ad37ac6
Matthias Koenig <konigmatt@googlemail.com>
2014-05-27 19:08:01 +0200

    Testing libSBML R swig fix

---
+++ R/TestLibSBML.R
@@ -0,0 +1,25 @@
+library('libSBML')
+filename = "/home/mkoenig/glucose-model/sbml/Koenig2014_Hepatic_Glucose_Model_annotated.xml"
+doc        = readSBML(filename);
+errors   = SBMLDocument_getNumErrors(doc);
+SBMLDocument_printErrors(doc);
+model = SBMLDocument_getModel(doc);
+lofp <- Model_getListOfParameters(model)
+
+# does not exist
+p <- ListOfParameters_get(lofp, 'asfasf')
+res <- tryCatch(SBase_getId(p), error = function(e) e)
+print(res)
+print(class(res))
+
+# exists
+p <- ListOfParameters_get(lofp, 'HGP')
+res <- tryCatch(SBase_getId(p), error = function(e) e)
+print(res)
+print(class(res))
+
+# direct way
+p$ref
+
+
+
