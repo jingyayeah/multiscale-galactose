c5ddb18136e6c1d4c7f01a859917d578b80c8a30
Matthias Koenig <konigmatt@googlemail.com>
2014-07-11 10:49:08 +0200

    Working on simulation assignment

--- python/simulator/simulation/test/QuerySetTest.py
+++ python/simulator/simulation/test/QuerySetTest.py
@@ -13,9 +13,21 @@ if __name__ == "__main__":
     print 'Test QuerySet'
     tasks = Task.objects.all()
     print tasks
+    print [t.priority for t in tasks]
     
     # sort by priority
     
+    # use value of the related tables
+    # problems with multiplicity
+    # ? could be count() a solution ?
+    tasks = Task.objects.filter(simulation__status=UNASSIGNED)
+    print tasks
+    
+    print tasks.distinct()
+    
+    # also the select for update
+    
+    # put it in a view? probably best? assign simulation view
     
     
     
