594ae3c0ed9345e31f4572c69a33cf34764d3ba2
Matthias Koenig <konigmatt@googlemail.com>
2014-07-30 11:41:49 +0200

    db server changed

--- python/modelcreator/modelcreator/models/BasicClearanceFactory.py
+++ python/modelcreator/modelcreator/models/BasicClearanceFactory.py
@@ -24,7 +24,7 @@ def createClearanceDilutionEventData(time_start, duration):
 if __name__ == "__main__":
 
     # definition of cell model and tissue model
-    Nc = 20
+    Nc = 1
     version = 3
     cell_model = CellModel.createModel('clearance.BasicClearanceCell')
     tdict = TissueModel.createTissueDict(['SinusoidalUnit', 
