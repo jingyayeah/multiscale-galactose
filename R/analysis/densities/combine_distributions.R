b3d78421e751e83deb23b81ad7cadc17c04de76b
Matthias Koenig <konigmatt@googlemail.com>
2014-11-05 18:36:44 +0100

    Rejection sampling for Monte-Carlo basic example

--- R/analysis/densities/combine_distributions.R
+++ R/analysis/densities/combine_distributions.R
@@ -3,7 +3,7 @@
 # In addition to random number generation, it'll get you the density, 
 # distribution, and quantile functions associated with your distribution:
 rm(list=ls())
-install.packages('distr')
+# install.packages('distr')
 library(distr)
 ## For more info, type: vignette("newDistributions")  
 
@@ -30,6 +30,9 @@ rD(20)
 # necessary to have quantile function and use *inverse transform sampling*
 
 
+##########################################
+# Density curves
+##########################################
 # Combining the information from different 
 # sample from combined probability densities
 
@@ -43,34 +46,126 @@ f_p2 <- function(x, mean=1200, sd=150){
 f_p3 <- function(x, mean=1100, sd=150){
   return(dnorm(x, mean, sd)) 
 }
+
+# problem: not a probability distribution (normalization to 1)
+# still missing
 f_pc <- function(x){
-  return( f_p1(x) * f_p2(x) * f_p3(x) ) 
+  return( f_p1(x) * f_p2(x) * f_p3(x)  ) 
   #return( f_q1(p)*f_q2(p) ) 
 }
 x <- volLiver.grid
 y1 <- f_p1(x=x)
 y2 <- f_p2(x=x)
-y2 <- f_p3(x=x)
+y3 <- f_p3(x=x)
 y_pc <- f_pc(x=x)
-
-
 plot(x, y1/max(y1), col='black', type='l', lty=2, ylim=c(0, 1.0))
 points(x, y2/max(y2), col='black', type='l', lty=3)
 points(x, y3/max(y3), col='black', type='l', lty=4)
 points(x, y_pc/max(y_pc), col='red', type='l', lty=1, lwd=4)
 legend('topright', legend=c('info A', 'info B', 'info C', 'combined'), col=c('black', 'black', 'black', 'red'), lty=c(2, 3,4,1))
-par(mfrow=c(1,1))
 
-n <- 1000
-yr1 <- rnorm(n, mean=50,sd=20)
-yr2 <- rnorm(n, mean=40, sd=10)
-yr3 <- yr1*yr2
+# normalization via integration over suitable range
+A <- integrate(f=f_pc, lower=0, upper=5000)
+A
+summary(A)
+A$value
+f_pc_normalized <- function(x, area=A$value) {
+  return (f_pc(x)/area)
+}
+y_pc_norm <- f_pc_normalized(x=x)
 
-par(mfrow=c(3,1))
-hist(yr1, breaks=20, xlim=c(-10, 110), col=rgb(0.5, 0.5, 0.5, 0.25))
-hist(yr2, breaks=20, xlim=c(-10, 110), col=rgb(0, 0, 1.0, 0.25), add=F)
-hist(yr3, breaks=20, xlim=c(-10, 110), col=rgb(1.0, 0, 0, 0.25), add=F)
-par(mfrow=c(1,1))
+# check the integral
+B <- integrate(f=f_pc_normalized, lower=500, upper=2000)
+B
+
+plot(x, y1, col='black', type='l', lty=2, ylim=c(0, max(y1)*4))
+points(x, y2, col='black', type='l', lty=3)
+points(x, y3, col='black', type='l', lty=4)
+points(x, y_pc_norm, col='red', type='l', lty=1, lwd=4)
+legend('topright', legend=c('info A', 'info B', 'info C', 'combined'), col=c('black', 'black', 'black', 'red'), lty=c(2, 3,4,1))
+
+# Sampling from an Arbitrary Density
+# http://blog.quantitations.com/tutorial/2012/11/20/sampling-from-an-arbitrary-density/
+
+# Inverse transform sampling
+invcdf <- function(u, m) {
+  return(sqrt(m^2/(1 - (1 - m^2) * u)))
+}
+sample1 <- sapply(runif(100), invcdf, m = .5)
+plot(density(sample1), main = "Sample Density using invcdf Function")
+
+# The R code below uses some of R’s built-in numerical methods to accomplish the inverse transform sampling technique for any arbitrary pdf that it is given. I’m sure there are some ugly pdfs for which this function wouldn’t work, but it works fine for typical densities.
+endsign <- function(f, sign = 1) {
+  b <- sign
+  while (sign * f(b) < 0) b <- 10 * b
+  return(b)
+}
+
+samplepdf <- function(n, pdf, ..., spdf.lower = -Inf, spdf.upper = Inf) {
+  vpdf <- function(v) sapply(v, pdf, ...)  # vectorize
+  cdf <- function(x) integrate(vpdf, spdf.lower, x)$value
+  invcdf <- function(u) {
+    subcdf <- function(t) cdf(t) - u
+    if (spdf.lower == -Inf) 
+      spdf.lower <- endsign(subcdf, -1)
+    if (spdf.upper == Inf) 
+      spdf.upper <- endsign(subcdf)
+    return(uniroot(subcdf, c(spdf.lower, spdf.upper))$root)
+  }
+  sapply(runif(n), invcdf)
+}
+
+sample2 <- samplepdf(100, f_pc_normalized)
+sample3 <- samplepdf(100, f_pc_normalized, spdf.lower = 500, spdf.upper = 2000)
+plot(density(sample2), main = "Sample Density using samplepdf Function")
+
+# Another option for sampling that doesn’t require finding the inverse cdf is rejection sampling.
+# http://playingwithr.blogspot.de/2011/06/rejection-sampling.html
+sample.x = runif(10000,0,1)
+accept = c()
+for(i in 1:length(sample.x)){
+  U = runif(1, 0, 1)
+  if(dunif(sample.x[i], 0, 1)*3*U <= dbeta(sample.x[i], 6, 3)) {
+    accept[i] = 'Yes'
+  }
+  else if(dunif(sample.x[i],0,1)*3*U > dbeta(sample.x[i], 6, 3)) {
+    accept[i] = 'No'
+  }
+}
+T = data.frame(sample.x, accept = factor(accept, levels= c('Yes','No')))
+head(T)
+# plot the results
+hist(T[,1][T$accept=='Yes'], breaks = seq(0,1,0.02), freq = FALSE, main = 'Histogram of X', xlab = 'X')
+x <- seq(0,1, length.out = 100)
+lines(x, dbeta(x,6,3))
+
+
+# rejection sampling for the given distribution
+# plot the distribution function and upper function for rejection sampling
+plot(volLiver.grid, f_pc_normalized(volLiver.grid))
+points( volLiver.grid, 5*dnorm(volLiver.grid, mean=1200, sd=400))
+
+sample.x = runif(50000,0,3000)
+accept = c()
+for(i in 1:length(sample.x)){
+  U = runif(1, 0, 1)
+  if(5*dnorm(sample.x[i], mean=1200, sd=400)*U <= f_pc_normalized(sample.x[i])) {
+    accept[i] = 'Yes'
+  }
+  else if(5*dnorm(sample.x[i], mean=1200, sd=400)*U > f_pc_normalized(sample.x[i])) {
+    accept[i] = 'No'
+  }
+}
+T = data.frame(sample.x, accept = factor(accept, levels= c('Yes','No')))
+head(T)
+summary(T)
+
+# plot the results
+hist(T[,1][T$accept=='Yes'], breaks = seq(0,3000,30), freq = FALSE, main = 'Histogram of X', xlab = 'X')
+lines(volLiver.grid, f_pc_normalized(volLiver.grid))
+
+
+
 
 ######################################
 # cummulative distribution functions
