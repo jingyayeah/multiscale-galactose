05d0a9b0468d40150c5b0ed224896e8d25913fe8
Matthias Koenig <konigmatt@googlemail.com>
2014-11-15 12:25:14 +0100

    Update of RoadRunner to 1.26

--- buildRoadrunner.sh
+++ buildRoadrunner.sh
@@ -9,10 +9,13 @@
 # install dependencies
 sudo apt-get install llvm python-numpy python-scipy
 
-CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/cpplibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
+#CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/cpplibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
+CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.6/cpplibroadrunner-1.2.6-linux_x86_64.tar.gz/download
+
 CPP_RR_NAME=cpplibroadrunner-latest
 
-PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/pylibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
+#PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/pylibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
+PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.6/pylibroadrunner-1.2.6-linux_x86_64.tar.gz/download
 PY_RR_NAME=pylibroadrunner-latest
 
 # get the latest files
