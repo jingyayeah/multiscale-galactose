9570faacf2e41a239626c6c011750d8e8988b2a8
Matthias Koenig <konigmatt@googlemail.com>
2014-11-18 09:02:30 +0100

    Adding experimental data Cattermole

--- R/analysis/data_analysis/Marchesini_data.R
+++ R/analysis/data_analysis/Marchesini_data.R
@@ -13,18 +13,27 @@ gender.cols = c("black", "blue", "red")
 f_liver_density = 1.08  # [g/ml] conversion between volume and weight
 
 # sex, age, liverVolume
-marG2 <- read.csv(file.path(ma.settings$dir.expdata, "Marchesini", "Koenig_G2.csv"), sep="\t")
+marG2 <- read.csv(file.path(ma.settings$dir.expdata, "marchesini", "Koenig_G2.csv"), sep="\t")
 marG2$gender <- as.character(marG2$sex)
 marG2$gender[marG2$gender=='M'] <- 'male'
 marG2$gender[marG2$gender=='F'] <- 'female'
 marG2$gender[marG2$gender=='A'] <- 'all'
-
 head(marG2)
 
 library(ggplot2)
-plot(marG2$age, marG2$GEC, type='n') 
+par(mfrow=c(2,1))
+plot(marG2$age, marG2$GEC, type='n', xlim=c(0,100), ylim=c(0,10)) 
 points(marG2$age[marG2$gender=='male'], marG2$GEC[marG2$gender=='male'], col='Blue') 
 points(marG2$age[marG2$gender=='female'], marG2$GEC[marG2$gender=='female'], col='Red') 
+
+plot(marG2$age, marG2$GECkg, type='n', xlim=c(0,100), ylim=c(0, 0.2))
+points(marG2$age[marG2$gender=='male'], marG2$GECkg[marG2$gender=='male'], col='Blue') 
+points(marG2$age[marG2$gender=='female'], marG2$GECkg[marG2$gender=='female'], col='Red') 
+
+par(mfrow=c(1,1))
+
+
+
 
 summary(marG2$type)
 plot(marG2$age, marG2$GEC, type='n') 
