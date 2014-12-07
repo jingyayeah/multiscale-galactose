369c76201f8126bbd54d4dcbf57f2b966f96f5b4
Matthias Koenig <konigmatt@googlemail.com>
2014-12-07 16:09:06 +0100

    Testing the RoadRunner problem

--- buildRoadrunner.sh
+++ buildRoadrunner.sh
@@ -9,12 +9,12 @@
 # install dependencies
 sudo apt-get install llvm python-numpy python-scipy
 
-#CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/cpplibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
+# CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/cpplibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
 CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.6/cpplibroadrunner-1.2.6-linux_x86_64.tar.gz/download
 
 CPP_RR_NAME=cpplibroadrunner-latest
 
-#PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/pylibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
+# PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/pylibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
 PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.6/pylibroadrunner-1.2.6-linux_x86_64.tar.gz/download
 PY_RR_NAME=pylibroadrunner-latest
 
@@ -32,7 +32,8 @@ sudo python setup.py install
 
 cd ~/Downloads
 tar xzvf ${PY_RR_NAME}.tar.gz
-cd pylibroadrunner-1.2.3-linux_x86_64
+# cd pylibroadrunner-1.2.3-linux_x86_64
+cd pylibroadrunner-1.2.6-linux_x86_64
 sudo python setup.py install
 
 cd ~/multiscale-galactose
