31f7af2027c6811ac3c3aea9dbcc2ea0225c7585
Matthias Koenig <konigmatt@googlemail.com>
2014-12-09 15:04:15 +0100

    Roadrunner simulations from spyder to demonstrate the model

--- python/simulator/simulator/spyder/spyder_test.py
+++ python/simulator/simulator/spyder/spyder_test.py
@@ -36,7 +36,7 @@ def set_inits(r, inits):
         r.model[name] = value 
     return changed
     
-def simulation(r, selection, parameters, inits):
+def simulation(r, selection, parameters, inits, absTol=1E-6, relTol=1E-6):
     # reset the initial concentrations
     r.reset()
     # set selection
@@ -47,8 +47,8 @@ def simulation(r, selection, parameters, inits):
     set_inits(r, inits)
         
     print '*** simulate ***'    
-    absTol = 1E-6 * min(r.model.getCompartmentVolumes())
-    relTol = 1E-6
+    # absTol is defined relative to the amounts
+    absTol = absTol * min(r.model.getCompartmentVolumes())
     start = time.clock()
     s = r.simulate(0, 5000, absolute=absTol, relative=relTol, variableStep=True, stiff=True, plot=False)    
     print 'Integration time:', (time.clock()- start)
@@ -72,7 +72,46 @@ def plot(r, show=True):
         p.legend()
     if show:
         p.show()
+
+
+def position_in_list(list, y):
+    for k, x in enumerate(list):
+        if x == y:
+            return k
+    return -1
+
+def dilution_plots(s_list, selections, show=True):
+    ''' Plot of the dilution curves '''
+    compounds = ['gal', 'galM', 'rbcM', 'alb', 'suc', 'h2oM']
+    ccols = ['gray', 'black', 'red', 'darkgreen', 'darkorange', 'darkblue']
+    pp_ids = ['PP__{}'.format(id) for id in compounds]    
+    pv_ids = ['PV__{}'.format(id) for id in compounds]
+    ids = pp_ids + pv_ids
+    cols = ccols + ccols
     
+    import pylab as p
+
+    # plot all the individual solutions    
+    for s in s_list:
+        time = s[:,0]
+        for k, id in enumerate(ids):
+            # find in which place of the solution the component is encoded
+            i_sel = position_in_list(selections, '[{}]'.format(id))
+            if i_sel < 0:
+                raise Exception("{} not in selection".format(id))
+            series = s[:,i_sel]
+            name = selections[i_sel]
+            p.plot(time, series, color=cols[k], label=str(name))
+            # p.legend()
+    # adapt the axis
+
+    p.xlim(999, 1015)
+    p.ylim(0, 0.4)
+
+    
+    if show:
+        p.show()
+
 #########################################################################    
 # Galactose Challenge
 #########################################################################    
@@ -102,6 +141,7 @@ plot(r)
 sbml_file = 'Galactose_v43_Nc20_dilution.xml'
 r = load_model(sbml_file)
 print r.model.items()
+items = r.model.items()
 
 # set selection
 sel = ['time']
@@ -111,16 +151,21 @@ sel += [ "".join(["[", item, "]"]) for item in ['PV__alb', 'PV__gal', 'PV__galM'
 # Store reactions
 # sel += [item for item in rr.model.getReactionIds() if item.startswith('H')]
 
-parameters = { 
-        "gal_challenge" : 8.0,
-    }
-inits = {"PP__gal" : 8.0,}
 
 # set the boundary concentrations
 # PP__gal = (0.28, 5, 12.5, 17.5) # [mM]
+p_list = [
+    { "[PP__gal]" : 0.28, },
+    { "[PP__gal]" : 12.5, },
+    { "[PP__gal]" : 17.5, }
+]
+inits = {}
 
-s1 = simulation(r, sel, parameters, inits)
-plot(r)
+# s1 = simulation(r, sel, p1, inits)
+s_list = [simulation(r, sel, p, inits, absTol=1E-3, relTol=1E-3) for p in p_list ]
+dilution_plots(s_list, r.selections)
 
+# additional changes for fitting the dilution curves
+(now test the effects of changing variables in the model, i.e.
 
 
