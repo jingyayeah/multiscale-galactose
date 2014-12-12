6ff6c0073ec24d80aa23e05935b096fae294685c
Matthias Koenig <konigmatt@googlemail.com>
2014-12-12 08:52:12 +0100

    Redefinition of simulations

--- python/simulator/simulator/SimulationFactory.py
+++ python/simulator/simulator/SimulationFactory.py
@@ -434,7 +434,7 @@ if __name__ == "__main__":
         
         
         # basic dilution curves with additional galactose challenge
-        [task, raw_samples] = make_galactose_dilution(sbml_id, N=1000, sampling="distribution")
+        [task, raw_samples] = make_galactose_dilution(sbml_id, N=500, sampling="distribution")
         samples = setParameterValuesInSamples(raw_samples, p_list)
         createSimulationsForSamples(task, samples)
         
