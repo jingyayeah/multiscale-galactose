5689bf77691f33b1e359631230f163f605341ebd
Matthias Koenig <konigmatt@googlemail.com>
2015-01-27 18:13:49 +0100

    Working on GEC prediction ROC curve

--- R/analysis/GEC_log_regression.R
+++ R/analysis/GEC_log_regression.R
@@ -63,11 +63,17 @@ for (k in 1:length(names)){
 }
 library('reshape')
 df <- reshape::merge_all(df.list)
-# create the classification outcome
+
+# create the classification outcome (liver disease) & check that binary classifier
 df$status <- as.factor(df$status)
 df$disease = as.numeric(df$status != 'healthy')
 summary(df)
 table(df$disease)
+# rename gender -> sex
+names(df)[names(df)=='gender'] = 'sex'
+table(df$sex)
+
+
 
 # plot overview over the available data
 par(mfrow = c(1,2))
@@ -110,14 +116,80 @@ plot(fitperf,col="darkgreen",lwd=2,main="ROC Curve for Logistic:  GEC")
 abline(a=0,b=1,lwd=2,lty=2,col="gray")
 par(mfrow = c(1,1))
 
+x.values = c(0.03030303, 0.07070707, 0.09090909, 0.1515152, 0.2020202, 0.2828283, 0.3838384)
+y.values = c(0.6153846, 0.7692308, 0.8461538, 0.8461538, 0.8461538,  1.0, 1.0)
+points(x.values, y.values, pch=22, col='black', bg='red')
+lines(x.values, y.values, col='red')
+
 #########################################################
 # Make the predictions based on the GEC App
 #########################################################
-person <- df[2,]
-person
-# now predict the range for the person
+# prediction of GEC range for the persons
+
+# GAMLSS models
+fit.models <- load_models_for_prediction()
+# GEC function
+GEC_f <- GEC_functions(task='T1')
 
 
+# make the predictions
+liver.info <- predict_liver_people(df, Nsample=2000, Ncores=1, debug=TRUE)
+GEC.info <- calculate_GEC(GEC_f, 
+                          volLiver=liver.info$volLiver,
+                          flowLiver=liver.info$flowLiver)
+GEC <- GEC.info$values
+
+# Calculate disease status based on the simulated GEC
+# data and the actual experimental value.
+predict_disease_status <- function(GECexp, GEC, probs=c(0.025, 0.05, 0.1, 0.15, 0.2, 0.3, 0.4)){
+    disease <- matrix(NA, nrow=length(GECexp), ncol=length(probs))
+    colnames(disease) = probs
+    
+    # predict every row
+    for (k in 1:length(GECexp)){
+      q <- quantile(GEC[k, ], probs=probs)
+      # cat('q=', q, '\tGEC=', GECexp[k], '\n')
+    
+      if (!is.na(GECexp[k])){
+        disease[k, ] = as.numeric(GECexp[k]<q)
+      }
+    }
+    return(list(disease=disease, probs=probs))
+}
+res <- predict_disease_status(df$GEC, GEC.info$values)
+res
+head(res$disease, 20)
+pred <- as.data.frame(res$disease)
+index <- complete.cases(pred)
+index
+
+# Now predicted and calculate values which can be used for the contingency table
+
+
+x.values = c(0.03030303, 0.07070707, 0.09090909, 0.1515152, 0.2020202, 0.2828283, 0.3838384)
+y.values = c(0.6153846, 0.7692308, 0.8461538, 0.8461538, 0.8461538,  1.0, 1.0)
+plot(x.values, y.values)
+
+
+
+real <- matrix( rep(df$disease[index], ncol(pred)), ncol=ncol(pred) )
+
+fitpred = prediction(pred[index, ], real)
+fitperf = performance(fitpred,"tpr","fpr")
+fitperf
+
+attr(fitperf, 'x.values')
+
+plot(attr(fitperf, 'x.values'),attr(fitperf, 'y.values'), col="darkgreen",lwd=2,main="ROC Curve for Logistic:  GEC")
+
+
+hist(GEC)
+abline(v=p1$GEC, lwd=2, col="red")
+quantile(GEC, probs = c(0.05))
+
+
+
+p1
 
 
 #########################################################
@@ -133,6 +205,7 @@ summary(fit2)
 
 # http://rocr.bioinf.mpi-sb.mpg.de/
 fitpreds = predict(fit2, newdata=df, type="response")
+
 fitpred = prediction(fitpreds, df$healthy)
 fitperf = performance(fitpred,"tpr","fpr")
 
