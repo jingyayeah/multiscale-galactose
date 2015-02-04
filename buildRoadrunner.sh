d053c37545a78cb4c83e4c735b61ffb09ac94fe3
Matthias Koenig <konigmatt@googlemail.com>
2015-02-04 12:51:36 +0100

    Update to RoadRunner v1.3

--- buildRoadrunner.sh
+++ buildRoadrunner.sh
@@ -10,15 +10,15 @@
 sudo apt-get install llvm python-numpy python-scipy
 
 # CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/cpplibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
-CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.6/cpplibroadrunner-1.2.6-linux_x86_64.tar.gz/download
-
-
+# CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.6/cpplibroadrunner-1.2.6-linux_x86_64.tar.gz/download
+CPP_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.3/cpplibroadrunner-1.3.0-linux_x86_64.tar.gz/download
 CPP_RR_NAME=cpplibroadrunner-latest
 
 # PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.3/pylibroadrunner-1.2.3-linux_ubuntu10.04%2B_x86_64.tar.gz/download
 # PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.2.6/pylibroadrunner-1.2.6-linux_x86_64.tar.gz/download
-#PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.3-beta/pylibroadrunner-1.3-symcache-beta5-linux_x86_64.tar.gz/download
-PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.3-beta/pylibroadrunner-1.3-beta8-linux_x86_64.tar.gz/download
+# PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.3-beta/pylibroadrunner-1.3-symcache-beta5-linux_x86_64.tar.gz/download
+# PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.3-beta/pylibroadrunner-1.3-beta8-linux_x86_64.tar.gz/download
+PY_RR=http://sourceforge.net/projects/libroadrunner/files/libroadrunner-1.3/pylibroadrunner-1.3-linux_x86_64.tar.gz/download
 
 PY_RR_NAME=pylibroadrunner-latest
 
@@ -40,7 +40,8 @@ tar xzvf ${PY_RR_NAME}.tar.gz
 # cd pylibroadrunner-1.2.3-linux_x86_64
 # cd pylibroadrunner-1.2.6-linux_x86_64
 # cd pylibroadrunner-1.3-symcache-beta5-linux_x86_64
-cd pylibroadrunner-1.3-beta8-linux_x86_64
+# cd pylibroadrunner-1.3-beta8-linux_x86_64
+cd pylibroadrunner-1.3-linux_x86_64
 
 sudo python setup.py install
 
