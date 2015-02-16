11765a06285762118b614888bcf805feabb1b682
Matthias Koenig <konigmatt@googlemail.com>
2015-02-16 17:21:26 +0100

    Added clearance & GE correction for systematic galactose clearance

--- R/analysis/GEC_curves_experiments.R
+++ R/analysis/GEC_curves_experiments.R
@@ -4,9 +4,6 @@
 # author: Matthias Koenig
 # date: 2015-02-12
 ################################################################
-# TODO: add error bars were available (Keiding1988)
-# TODO: correction of all GE & CL values, when not based on ca-cv differences
-
 rm(list=ls())
 library('MultiscaleAnalysis')
 setwd(file.path(ma.settings$dir.exp, 'GEC'))
@@ -20,7 +17,7 @@ do_plot = TRUE
 # similar removal kinetics for the extrahepatic tissues than for the liver
 # (also cleared by galactokinase)
 calculate_f_Rbase <- function(){
-  GALK_km = 0.5  # [mM] resulting kinetics of galactose elimination
+  GALK_km = 0.2  # [mM] resulting kinetics of galactose elimination
   gal_eq = 0.113  # [mM] (Keiding1988)
   Rb_eq = 41      # [µmol/min] Basal rate at gal_eq (Keiding1988)
   Vmax_Rb = Rb_eq * (gal_eq + GALK_km)/gal_eq/1000 # [mmol/min]
@@ -48,8 +45,6 @@ plot(gal, Rbase,
      font.lab=2)
 lines(gal, Rbase)
 
-
-
 ########################################################################
 # Combined data (GE, ER, CL)
 ########################################################################
@@ -62,11 +57,9 @@ wal1960 <- read.csv(file.path(ma.settings$dir.exp, 'GEC', "Waldstein1960_Tab1.cs
 hen1982 <- read.csv(file.path(ma.settings$dir.exp, 'GEC', "Henderson1982_Tab4.csv"), sep="\t")
 hen1982 <- hen1982[hen1982$status == 'healthy', ]
 hen1982.tab2 <- read.csv(file.path(ma.settings$dir.exp, 'GEC', "Henderson1982_Tab2.csv"), sep="\t")
-head(hen1982.tab2)
 win1965 <- read.csv(file.path(ma.settings$dir.exp, 'GEC', "Winkler1965.csv"), sep="\t")
-head(win1965)
 
-# Which data has to be corrected?
+# Data has to be corrected for systemic galactose clearance
 # Necessary to correct the actual Rhep=GE & Clearance calculated from it
 # (no) kei1988 (Clearance based on ca-co, femoral artery)
 # (no) win1965 (CL & GE via ca-co difference, femoral artery)
@@ -76,24 +69,48 @@ head(win1965)
 # still measuring systemic clearance & hepatic clearance
 
 # (yes) wal1960 (GE & CLH via infusion rate, and periphereal c)
-# (yes CL, no GE) tyg1958 (Clearance based on I/ci, GE via ca-co) 
 # (yes) tyg1954 (CL & GE estimated via cp-co)
+# (yes CL, GE) tyg1958 (Clearance based on I/ci, GE via ca-co) 
 # (yes) hen1982 (CL & GE vi infusion rate)
 
 # Correction wal1960
-head(wal1960)
-names(wal1960)
-wal1960$Rbase <- calculate_Rbase(wal1960$Peq)
+wal1960$Rbase <- calculate_Rbase(wal1960$gal)
 wal1960$GEcor = wal1960$R - wal1960$Rbase
 wal1960$CLcor = wal1960$CLH - wal1960$Rbase/wal1960$gal*1000 
 head(wal1960)
 wal1960$CLcor/wal1960$CLH
 
-# Errorbars available for
+# Correction tyg1954
+head(tyg1954)
+tyg1954$Rbase <- calculate_Rbase(tyg1954$ca)
+tyg1954$GEcor = tyg1954$GEEst - tyg1954$Rbase
+tyg1954$CLcor = tyg1954$CLEst - tyg1954$Rbase/tyg1954$ca*1000 
+tyg1954$CLcor/tyg1954$CLEst
+
+# Correction tyg1958
+head(tyg1958)
+tyg1958$Rbase <- calculate_Rbase(tyg1958$ca)
+tyg1958$GEcor = tyg1958$GE - tyg1958$Rbase
+tyg1958$CLcor = tyg1958$CL - tyg1958$Rbase/tyg1958$ca*1000 
+tyg1958$CLcor/tyg1958$CL
+
+# Correction hen1982
+head(hen1982)
+hen1982$Rbase <- calculate_Rbase(hen1982$css)
+hen1982$GEcor = hen1982$GE - hen1982$Rbase
+hen1982$CLcor = hen1982$CL - hen1982$Rbase/hen1982$css*1000 
+hen1982$CLcor/hen1982$CL
+
+head(hen1982.tab2)
+hen1982.tab2$Rbase <- calculate_Rbase(hen1982.tab2$css)
+hen1982.tab2$GEcor = hen1982.tab2$GE - hen1982.tab2$Rbase
+hen1982.tab2$CLcor = hen1982.tab2$CL - hen1982.tab2$Rbase/hen1982.tab2$css*1000 
+hen1982.tab2$CLcor/hen1982.tab2$CL
+
+# Errorbars available in the following datasets
 # kei1988 (ca, cv, ER, GE)
 # wal1960 (Peq-> ca)
 
-# ? is this needed
 exp <- list(
  kei1988=kei1988,
  tyg1958=tyg1958,
@@ -103,8 +120,6 @@ exp <- list(
  win1965=win1965
 )
 
-
-
 exp_pchs <- rep(22,length(exp))
 names(exp_pchs) <- names(exp)
 exp_bg <- c('red', 'darkgreen', 'orange', 'blue', 'brown', rgb(0.3, 0.3, 0.3))
@@ -112,36 +127,15 @@ names(exp_bg) <- names(exp)
 exp_cols <- c('red', 'darkgreen', 'orange', 'blue', 'brown', rgb(0.3, 0.3, 0.3))
 names(exp_cols) <- names(exp)
 
-
-
 add_exp_legend <- function(loc="topleft", subset){
   legend(loc, legend=gsub("19", "", subset), col=exp_cols[subset], pt.bg=exp_bg[subset], pch=exp_pchs[subset], cex=0.8, bty='n')
 }
-
 
 if (do_plot){
   fname <- file.path(ma.settings$dir.base, 'results', 'Galactose_elimination_experiments.png')
   png(filename=fname, width=1800, height=1000, units = "px", bg = "white",  res = 120)
 }
-
-
-plot_errorbars <- function(x, xSE=NA, y, ySE=NA, col='gray'){
-  
-  # plot horizontal
-  if (!is.na(xSE)){
-    for(i in 1:length(x)) {
-      
-      segments(x[i],low , x[i], up)
-      
-    }  
-  }
-  
-}
-
-
 par(mfrow=c(2,4))
-
-par(mfrow=c(1,1))
 
 # [1] Galactose elimination
 # -----------------------------------------------------
@@ -151,7 +145,7 @@ plot(numeric(0), numeric(0), type='n', font.lab=2,
      ylab="Galactose elimination [mmol/min]",
      xlim=c(0, 3200), 
      ylim=c(0, 3))
-points(tyg1958$bloodflowBS, tyg1958$GE, 
+points(tyg1958$bloodflowBS, tyg1958$GEcor, 
        bg=exp_bg[["tyg1958"]], col=exp_cols[["tyg1958"]], pch=exp_pchs[["tyg1958"]]) 
 points(kei1988$bloodFlow, kei1988$HE,
        bg=exp_bg[["kei1988"]], col=exp_cols[["kei1988"]], pch=exp_pchs[["kei1988"]])
@@ -163,13 +157,11 @@ segments(kei1988$bloodFlow, kei1988$HE-kei1988$HESE,
          col=exp_cols[["kei1988"]])
 points(win1965$flowLiver, win1965$GE, 
        bg=exp_bg[["win1965"]], col=exp_cols[["win1965"]], pch=exp_pchs[["win1965"]])
-points(hen1982$bloodflow, hen1982$GE, 
+points(hen1982$bloodflow, hen1982$GEcor, 
        bg=exp_bg[["hen1982"]], col=exp_cols[["hen1982"]], pch=exp_pchs[["hen1982"]])
-points(hen1982$bloodflow, hen1982$GE, 
-       bg=exp_bg[["hen1982"]], col=exp_cols[["hen1982"]], pch=exp_pchs[["hen1982"]])
-points(tyg1954$bloodflowEst, tyg1954$GEEst, 
+points(tyg1954$bloodflowEst, tyg1954$GEcor, 
        bg=exp_bg[["tyg1954"]], col=exp_cols[["tyg1954"]], pch=exp_pchs[["tyg1954"]])
-lines(tyg1954$bloodflowEst, tyg1954$GEEst, col=exp_cols[["tyg1954"]])
+lines(tyg1954$bloodflowEst, tyg1954$GEcor, col=exp_cols[["tyg1954"]])
 add_exp_legend(subset=c("tyg1958","kei1988", "win1965", "hen1982", "tyg1954"))
 
 plot(numeric(0), numeric(0), type='n', font.lab=2,
@@ -177,7 +169,7 @@ plot(numeric(0), numeric(0), type='n', font.lab=2,
      ylab="Galactose elimination [mmol/min]",
      xlim=c(0, 10), 
      ylim=c(0, 3))
-points(tyg1958$ca, tyg1958$GE,
+points(tyg1958$ca, tyg1958$GEcor,
        bg=exp_bg[["tyg1958"]], col=exp_cols[["tyg1958"]], pch=exp_pchs[["tyg1958"]])
 points(wal1960$gal, wal1960$GEcor,
        bg=exp_bg[["wal1960"]], col=exp_cols[["wal1960"]], pch=exp_pchs[["wal1960"]])
@@ -194,11 +186,13 @@ segments(kei1988$bloodFlow, kei1988$HE-kei1988$HESE,
          col=exp_cols[["kei1988"]])
 points(win1965$ca, win1965$GE,
        bg=exp_bg[["win1965"]], col=exp_cols[["win1965"]], pch=exp_pchs[["win1965"]])
-points(hen1982$css, hen1982$GE, 
+points(hen1982$css, hen1982$GEcor, 
        bg=exp_bg[["hen1982"]], col=exp_cols[["hen1982"]], pch=exp_pchs[["hen1982"]])
-points(tyg1954$ca, tyg1954$GEEst, 
+points(hen1982.tab2$css, hen1982.tab2$GEcor, 
+       bg=exp_bg[["hen1982"]], col=exp_cols[["hen1982"]], pch=exp_pchs[["hen1982"]])
+points(tyg1954$ca, tyg1954$GEcor, 
        bg=exp_bg[["tyg1954"]], col=exp_cols[["tyg1954"]], pch=exp_pchs[["tyg1954"]])
-lines(tyg1954$ca, tyg1954$GEEst, col=exp_cols[["tyg1954"]])
+lines(tyg1954$ca, tyg1954$GEcor, col=exp_cols[["tyg1954"]])
 add_exp_legend("bottomright", subset=c("tyg1958","wal1960", "kei1988", "win1965", "hen1982", "tyg1954"))
 
 # [2] Galactose extraction ratio ER
@@ -261,7 +255,7 @@ plot(numeric(0), numeric(0), type='n', font.lab=2,
      ylab="Galactose clearance [ml/min]",
      xlim=c(0, 3200),
      ylim=c(0, 2600))
-points(tyg1958$bloodflowBS, tyg1958$CL,
+points(tyg1958$bloodflowBS, tyg1958$CLcor,
        bg=exp_bg[["tyg1958"]], col=exp_cols[["tyg1958"]], pch=exp_pchs[["tyg1958"]])
 points(kei1988$bloodFlow, kei1988$HCL,
        bg=exp_bg[["kei1988"]], col=exp_cols[["kei1988"]], pch=exp_pchs[["kei1988"]])
@@ -273,11 +267,11 @@ segments(kei1988$bloodFlow, kei1988$HCL-kei1988$HCLSE,
          col=exp_cols[["kei1988"]])
 points(win1965$flowLiver, win1965$CL,
        bg=exp_bg[["win1965"]], col=exp_cols[["win1965"]], pch=exp_pchs[["win1965"]])
-points(hen1982$bloodflow, hen1982$CL,
+points(hen1982$bloodflow, hen1982$CLcor,
        bg=exp_bg[["hen1982"]], col=exp_cols[["hen1982"]], pch=exp_pchs[["hen1982"]])
-points(tyg1954$bloodflowEst, tyg1954$CLEst, 
+points(tyg1954$bloodflowEst, tyg1954$CLcor, 
        bg=exp_bg[["tyg1954"]], col=exp_cols[["tyg1954"]], pch=exp_pchs[["tyg1954"]])
-lines(tyg1954$bloodflowEst, tyg1954$CLEst, col=exp_cols[["tyg1954"]])
+lines(tyg1954$bloodflowEst, tyg1954$CLcor, col=exp_cols[["tyg1954"]])
 add_exp_legend("bottomright", subset=c("tyg1958","kei1988", "win1965", "hen1982", "tyg1954"))
 
 plot(numeric(0), numeric(0), type='n', font.lab=2,
@@ -285,7 +279,7 @@ plot(numeric(0), numeric(0), type='n', font.lab=2,
      ylab="Galactose clearance [ml/min]",
      xlim=c(0, 10),
      ylim=c(0, 3200))
-points(tyg1958$ca, tyg1958$CL,
+points(tyg1958$ca, tyg1958$CLcor,
        bg=exp_bg[["tyg1958"]], col=exp_cols[["tyg1958"]], pch=exp_pchs[["tyg1958"]])
 points(kei1988$ca, kei1988$HCL,
        bg=exp_bg[["kei1988"]], col=exp_cols[["kei1988"]], pch=exp_pchs[["kei1988"]])
@@ -295,15 +289,15 @@ segments(kei1988$ca-kei1988$caSE, kei1988$HCL,
 segments(kei1988$ca, kei1988$HCL-kei1988$HCLSE,
          kei1988$ca, kei1988$HCL+kei1988$HCLSE,
          col=exp_cols[["kei1988"]])
-points(hen1982$css, hen1982$CL, 
+points(hen1982$css, hen1982$CLcor, 
        bg=exp_bg[["hen1982"]], col=exp_cols[["hen1982"]], pch=exp_pchs[["hen1982"]])
-points(hen1982.tab2$css, hen1982.tab2$CL, 
+points(hen1982.tab2$css, hen1982.tab2$CLcor, 
        bg=exp_bg[["hen1982"]], col=exp_cols[["hen1982"]], pch=exp_pchs[["hen1982"]])
 points(win1965$ca, win1965$CL,
        bg=exp_bg[["win1965"]], col=exp_cols[["win1965"]], pch=exp_pchs[["win1965"]])
-points(tyg1954$ca, tyg1954$CLEst, 
+points(tyg1954$ca, tyg1954$CLcor, 
        bg=exp_bg[["tyg1954"]], col=exp_cols[["tyg1954"]], pch=exp_pchs[["tyg1954"]])
-lines(tyg1954$ca, tyg1954$CLEst, col=exp_cols[["tyg1954"]])
+lines(tyg1954$ca, tyg1954$CLcor, col=exp_cols[["tyg1954"]])
 points(wal1960$gal, wal1960$CLcor,
        bg=exp_bg[["wal1960"]], col=exp_cols[["wal1960"]], pch=exp_pchs[["wal1960"]])
 segments(wal1960$gal-wal1960$galSd, wal1960$CLcor,
