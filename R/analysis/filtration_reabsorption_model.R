ddc958e3f6d5dc4a8134198f410842a0737937a4
Matthias Koenig <konigmatt@googlemail.com>
2015-04-09 17:31:16 +0200

    Refactoring the filtration_reabsorption calculation

--- R/analysis/filtration_reabsorption_model.R
+++ R/analysis/filtration_reabsorption_model.R
@@ -8,97 +8,21 @@
 # author: Matthias Koenig
 # date: 2014-03-05
 #---------------------------------------------------------------
+library('MultiscaleAnalysis')
 rm(list=ls())
 
-# [1] Kozlova2000 model
-# pressures
+# Create standard parameters from SBML model
+# TODO: read the necessary parameters from the SBML
+p_flow <- koz_parameters()
+p_flow$nu_f = 10
 
-
-# Create standard parameters for calculation
-p_flow <- list(
-  Pa = 10,            # [mmHg] portal pressure
-  Pb = 2.0,          # [mmHg] central pressure
-  
-  # viscosity, so that the actual blood flows are correct
-  nu_f = 10,            # [-] flow dependent viscosity in capillaries (~ Factor 3 at 100µm/s)
-  nu_plasma = 0.0018,   # [Pa*s]
-  
-  y_sin = 4.4E-6,  # [m]
-  L = 500E-6,  # [m]
-  y_end = 1.65E-7, # [m]
-  N_fen = 10E12,   # [1/m^2]
-  r_fen = 5.35E-8 # [m]
-)
 head(p_flow)
-
-
-# Additional derived parameters
-derived_pars <- function(p){
-  p$Pa_per_mmHg = 133.322  # [Pa/mmHg]
-  p$P0 = 0.5*(p$Pa+p$Pb)       # [mmHg] P0 = Poc-Pot, resulting oncotic pressure
-  p$nu = p$nu_plasma * p$nu_f  # [Pa*s]
-  
-  p$W = 8*p$nu/(pi*p$y_sin^4)            # [Pa*s/m^4] specific hydraulic resistance (blood)
-  p$w = 4*p$nu*p$y_end/(pi^2*p$r_fen^4*p$y_sin*p$N_fen)  # [Pa*s/m^2] hydraulic resistance of all pores (plasma)
-  
-  p$lambda = sqrt(p$w/p$W) # [m]
-  p$lambda2 = sqrt(p$y_sin^3*p$y_end/(2*pi*p$r_fen^4*p$N_fen)) #[m]
-  
-  if (abs(p$lambda-p$lambda2)>1E-8){
-    warning('Lambda calculation failed.')
-  }
-  return(p)
-}
-p_new <- derived_pars(p_flow)
-
-
-y = with(p_new, p_new$lambda)
-
-# solutions of the partial differential equations
-# P(x) [mmHg] - pressure in mmHg
-P_f <- function(x, p){
-  # returns pressure in units of Pa, Pb, P0, i.e. Pa
-  y <- with(p, 
-    (-(Pb-P0) + (Pa-P0)*exp(-L/lambda))/(exp(-L/lambda)-exp(L/lambda))*exp( x/lambda) + 
-    ( (Pb-P0) - (Pa-P0)*exp( L/lambda))/(exp(-L/lambda)-exp(L/lambda))*exp(-x/lambda) + P0
-  )
-  return(y)
-}
-
-# Q(x) flow along sinusoid in [m^3/s]
-Q_f <- function(x, p){
-  y <- with(p, {
-    p_x <- (-(Pb-P0) + (Pa-P0)*exp(-L/lambda))/(exp(-L/lambda)-exp(L/lambda))*exp( x/lambda) - 
-      ( (Pb-P0) - (Pa-P0)*exp( L/lambda))/(exp(-L/lambda)-exp(L/lambda))*exp(-x/lambda)
-    p_x <- p_x * Pa_per_mmHg # [Pa]
-    y <- - 1/sqrt(W*w) * p_x  # [m^3/Pa*s * Pa] = [m^3/s]
-    })
-  return(y)
-}
-
-# q(x) flow through pores [m^2/s]
-# why not volume flow ?
-q_f <- function(x, p){
-  y <- with(p, {
-    p_x <- (-(Pb-P0) + (Pa-P0)*exp(-L/lambda))/(exp(-L/lambda)-exp(L/lambda))*exp( x/lambda) + 
-      ( (Pb-P0) - (Pa-P0)*exp( L/lambda))/(exp(-L/lambda)-exp(L/lambda))*exp(-x/lambda)
-    p_x <- p_x * Pa_per_mmHg
-    y <- 1/w * p_x # [m^2/Pa*s * Pa] = [m^2/s]
-  })
-  return(y)
-}
-
-# flow velocity [m/s]
-v_f <- function(x, p){
-  Q <- Q_f(x,p) # [m^3/s]
-  A <- pi*p$y_sin^2
-  v <- Q/A
-  return(v)
-}
+p_new <- koz_derived_parameters(p_flow)
 
 # Figure: Pressure and flow profiles
-par(mfrow=c(3,1))
 x <- seq(from=0, to=p_new$L, length.out=40)
+par(mfrow=c(2,2))
+# pressure along capillary
 plot(x, P_f(x, p_new), type='l',
       font.lab=2,
       main='Pressure along capillary',
@@ -106,6 +30,7 @@ plot(x, P_f(x, p_new), type='l',
       xlim=c(0,p_new$L), ylim=c(0, p_new$Pa))
 abline(h=0)
 
+# flow along capillary
 plot(x, Q_f(x, p_new), type='l',
       font.lab=2,
       main='Flow along capillary',
@@ -113,15 +38,15 @@ plot(x, Q_f(x, p_new), type='l',
       xlim=c(0,p_new$L), ylim=c(0,1E-13))
 abline(h=0)
 
+# flow through pores
 plot(x, q_f(x, p_new), type='l',
       font.lab=2,
       main='Flow through pores',
       xlab='x [m]', ylab='q(x) [m^2/s]',
       xlim=c(0,p_new$L))
 abline(h=0)
-par(mfrow=c(1,1))
 
-# Flow velocity in capillary
+# blood flow velocity
 v = v_f(x, p_new)
 plot(x, v, type='l',
      font.lab=2,
@@ -131,7 +56,10 @@ plot(x, v, type='l',
      ylim=c(0,1E-3))
 abline(h=0)
 abline(h=mean(v), lty=2)
+par(mfrow=c(1,1))
+
 cat('y_flow = ', mean(v), ' [m/s]\n')
+cat('max/mean v_flow = ', max(v)/mean(v), ' [-]\n')
 
 
 #####################################################################
