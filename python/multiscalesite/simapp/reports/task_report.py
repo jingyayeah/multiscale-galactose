6057691d738815e3b6237070b7ac6cd2b0f153d1
Matthias Koenig <konigmatt@googlemail.com>
2015-05-21 08:29:15 +0200

    Database schema updated

--- python/multiscalesite/simapp/reports/task_report.py
+++ python/multiscalesite/simapp/reports/task_report.py
@@ -3,12 +3,17 @@ Creating report of the parameters of a given task.
 Parameters of a single task are collected.
 
 """
+from __future__ import print_function
+import pandas as pd
+from pandas import DataFrame
 
-# TODO: refactor 
+
+# TODO: refactor
 
 import time
 from simapp.models import Task
 
+# Manage the files
 def getParameterFilenameForTask(task, folder=None):
     """TODO: proper general paths"""
     if not folder:
@@ -17,12 +22,14 @@ def getParameterFilenameForTask(task, folder=None):
     mname = task.model.sbml_id
     return ''.join([folder, "/", str(task), "_", mname, "_parameters.csv"])
 
+
 def createParameterFileForTask(task, folder=None):
     fname = getParameterFilenameForTask(task, folder=folder)
     f = file(fname, 'w')
     f.write(createParameterStringInfoForTask(task))
     f.close()
     return fname
+
 
 def createParameterStringInfoForTask(task):
     data = createParameterInfoForTask(task)
@@ -33,54 +40,68 @@ def createParameterStringInfoForTask(task):
         lines +=  [", ".join([str(data[key][k]) for key in header])]
     return "\n".join(lines)
 
-def createParameterInfoForTask(task):
-    """
-    Write the parameter file for the task.
-    Simulation Id is stored in collection with the parameters.
-    The problem is that within a task there can be different
-    numbers of parameters set. It has to be guaranteed by the
-    SimulationFactory.
-    """
-    print 'Create Parameter File for: ', str(task)
-    start = time.clock()
-    # collect the parameters for the simulations
-    data = dict()
-    data['sim'] = []
-    data['status'] = []
-    data['core'] = []
-    data['duration'] = []
-    
-    # TODO: make this faster by getting the related information
-    for sim in task.simulations.all():
-        data['sim'].append(sim.pk)
-        data['status'].append(sim.status)
-        data['core'].append(sim.core)
-        data['duration'].append(sim.duration)
-        # add all the parameters
-        for p in sim.parameters.all():
-            if data.has_key(p.key):
-                data[p.key].append(p.value)
-            else:
-                data[p.key] = [p.value]
-    
-    # check that everything has the same length
-    # this has to be guaranteed by the odesim generator
-    for key in data.iterkeys():
-        if len(data[key]) != len(data['sim']):
-            print 'ERROR - wrong number of parameters'
-    
-    print 'time: ', (time.clock() - start)
-    return data
-        
 
+
+class TaskReport(object):
+    def __init__(self, task):
+        self.task = task
+
+    def create_parameter_dataframe(self):
+        """
+        Simulation pk is stored in collection with other parameters.
+        """
+        # get all the parameter names
+        header = set(['sim', 'status', 'core', 'duration'])
+        set.add()
+        d_sim = {}
+        for sim in self.task.simulations.all():
+            parameters = sim.parameters.all()
+            d_sim[sim] = parameters
+            header.add([p.key for p in parameters])
+
+        print(d_sim)
+        print(header)
+
+
+        '''
+        print 'Create Parameter File for: ', str(task)
+        start = time.clock()
+        # collect the parameters for the simulations
+        data = dict()
+        data['sim'] = []
+        data['status'] = []
+        data['core'] = []
+        data['duration'] = []
+
+
+        # TODO: make this faster by getting the related information
+        for sim in task.simulations.all():
+            data['sim'].append(sim.pk)
+            data['status'].append(sim.status)
+            data['core'].append(sim.core)
+            data['duration'].append(sim.duration)
+            # add all the parameters
+            for p in sim.parameters.all():
+                if data.has_key(p.key):
+                    data[p.key].append(p.value)
+                else:
+                    data[p.key] = [p.value]
+
+        # check that everything has the same length
+        # this has to be guaranteed by the odesim generator
+        for key in data.iterkeys():
+            if len(data[key]) != len(data['sim']):
+                print 'ERROR - wrong number of parameters'
+
+        print 'time: ', (time.clock() - start)
+        return data
+        '''
 
 if __name__ == "__main__":
     import django
     django.setup()
-    
-    # write the parameter files
-    folder = "/home/mkoenig/multiscale-galactose-results/"
-    ids = (1, 2 )
-    for task_id in ids:    
-        task = Task.objects.get(pk=task_id)
-        createParameterFileForTask(task, folder)
+
+    from simapp.models import Task
+    task = Task.objects.get(pk=32)
+    task_report = TaskReport(task)
+    task_report.create_parameter_dataframe()
