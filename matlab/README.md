71cb615aa96252e1d04f36897f9cc56c29bd76d9
Matthias Koenig <konigmatt@googlemail.com>
2015-11-25 15:17:27 +0100

    Single cell matlab simulations

---
+++ matlab/README.md
@@ -0,0 +1,12 @@
+## Matlab simulations
+# installation
+Setup all the paths for the simulations via
+```
+install_multiscale_galactose
+```
+
+# run simulations
+The simulations can be started via 
+```
+sim_sinusoid
+```
--- matlab/install_multiscale_galactose.m
+++ matlab/install_multiscale_galactose.m
@@ -1,8 +1,7 @@
 %% Initializes the scripts
 % Adds the necessary subfolders to the path.
+% Copyright Matthias Koenig 2013-2015 All Rights Reserved.
 
-%   Matthias Koenig (2014-03-10)
-%   Copyright Matthias Koenig 2013 All Rights Reserved.
 clear all, close all, clc
 fprintf('Installation Script\n');
 folder = pwd;
