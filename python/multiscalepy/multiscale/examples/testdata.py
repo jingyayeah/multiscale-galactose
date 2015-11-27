494be9d4bc044e25ab4584a406e0a7e1e0def588
Matthias Koenig <konigmatt@googlemail.com>
2015-11-27 16:58:20 +0100

    Starting with model annotation

--- python/multiscalepy/multiscale/examples/testdata.py
+++ python/multiscalepy/multiscale/examples/testdata.py
@@ -7,5 +7,5 @@ test_dir = os.path.dirname(os.path.abspath(__file__))
 
 demo_model_id = 'Koenig_demo_v02'
 demo_filepath = os.path.join(test_dir, 'models/demo', '{}.xml'.format(demo_model_id))
-
 csv_filepath = os.path.join(test_dir, 'data', 'test.csv')
+
