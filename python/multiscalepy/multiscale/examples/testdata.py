e14d450e80fb0c4f0dc44ed7f72585984e196559
Matthias Koenig <konigmatt@googlemail.com>
2016-01-11 09:29:33 +0100

    Updating python notebooks

--- python/multiscalepy/multiscale/examples/testdata.py
+++ python/multiscalepy/multiscale/examples/testdata.py
@@ -5,7 +5,6 @@ Managing the test data.
 import os
 test_dir = os.path.dirname(os.path.abspath(__file__))
 
-demo_model_id = 'Koenig_demo_v02'
-demo_filepath = os.path.join(test_dir, 'models/demo', '{}.xml'.format(demo_model_id))
+demo_filepath = os.path.join(test_dir, 'models/demo', 'Koenig_demo_v09.xml')
 csv_filepath = os.path.join(test_dir, 'data', 'test.csv')
 
