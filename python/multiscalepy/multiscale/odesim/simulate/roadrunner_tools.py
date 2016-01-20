fd1172973bd7f7c13f30cd51573732e937797408
Matthias Koenig <konigmatt@googlemail.com>
2016-01-20 18:54:04 +0100

    refactoring roadrunner_tools

--- python/multiscalepy/multiscale/odesim/simulate/roadrunner_tools.py
+++ python/multiscalepy/multiscale/odesim/simulate/roadrunner_tools.py
@@ -30,200 +30,221 @@ class MyRunner(RoadRunner):
     def __init__(self, *args, **kwargs):
         # super constructor
         RoadRunner.__init__(self, *args, **kwargs)
+        self.debug = False
+        # set the default settings for the integrator
+        self.set_default_settings()
 
-    simulate = time_it()(RoadRunner.simulate)
+    #########################################################################
+    # Settings
+    #########################################################################
+    def set_debug(self, debug):
+        self.debug = debug
 
+    def set_default_settings(self):
+        """ Set default settings of integrator. """
+        self.set_integrator_settings(
+                variable_step_size=True,
+                stiff=True,
+                absolute_tolerance=1E-8,
+                relative_tolerance=1E-8
+        )
 
+    def set_integrator_settings(self, **kwargs):
+        """ Set integrator settings. """
+        integrator = self.getIntegrator()
+        for key, value in kwargs.items():
+            # adapt the absolute_tolerance relative to the amounts
+            if key == "absolute_tolerance":
+                value = value * min(self.model.getCompartmentVolumes())
+            integrator.setValue(key, value)
 
-#########################################################################
-# Helper for units & selections
-#########################################################################
-def get_global_constant_parameters(r):
-    """ Subset of global parameters which are constant.
-        Set of parameter which has constant values and is not calculated
-        based on an initialAssignment.
-    """
-    # All global parameters which are constant have to be varied.
-    parameter_ids = r.model.getGlobalParameterIds()
-    import libsbml
-    doc = libsbml.readSBMLFromString(r.getSBML())
-    model = doc.getModel()
-    const_parameter_ids = []
-    for pid in parameter_ids:
-        p = model.getParameter(pid)
-        if p.constant:
-            const_parameter_ids.append(pid)
-    return const_parameter_ids
+        if self.debug:
+            print(integrator)
 
+    #########################################################################
+    # Selections
+    #########################################################################
+    def get_floating_concentration_selections(self):
+        return ['time'] + ['[{}]'.format(s) for s in self.model.getFloatingSpeciesIds()]
 
-#########################################################################
-# DataFrames
-#########################################################################
+    #########################################################################
+    # Simulation
+    #########################################################################
+    # supported simulate options
+    _simulate_args = frozenset(["start", "end", "steps", "selections"])
 
-def global_parameters_dataframe(r):
-    """ Create GlobalParameter DataFrame. """
-    return DataFrame({'value': r.model.getGlobalParameterValues()},
-                     index=r.model.getGlobalParameterIds())
+    @time_it()
+    def simulate(self, *args, **kwargs):
+        """ Timed simulate function. """
+        for key in kwargs:
+            assert(key in self._simulate_args)
+        return RoadRunner.simulate(*args, **kwargs)
 
+    def simulate_complex(self, initial_concentrations={}, initial_amounts={},
+                         parameters={}, reset_parameters=False, **kwargs):
+        """ Perform RoadRunner simulation.
+            Sets parameter values given in parameters dictionary &
+            initial values provided in dictionaries.
 
-def floating_species_dataframe(r):
-    """ Create FloatingSpecies DataFrame. """
-    return DataFrame({'concentration': r.model.getFloatingSpeciesConcentrations(),
-                      'amount': r.model.getFloatingSpeciesAmounts()},
-                     index=r.model.getFloatingSpeciesIds())
+            Optional argument:
+            parameters:
+                dictionary of parameter changes
 
+            reset_parameters:
+                reset the parameter changes after the simulation
 
-def boundary_species_dataframe(r):
-    """ Create BoundingSpecies DataFrame. """
-    return DataFrame({'concentration': r.model.getBoundarySpeciesConcentrations(),
-                      'amount': r.model.getBoundarySpeciesAmounts()},
-                     index=r.model.getBoundarySpeciesIds())
+            initial_concentrations:
+                dictionary of initial_concentrations
 
+            initial_concentrations:
+                dictionary of initial_amounts (overwrites initial concentrations)
 
-#########################################################################
-# Simulation
-#########################################################################
-def set_integrator_settings(r, variable_step_size=True, stiff=True,
-                            absolute_tolerance=1E-8, relative_tolerance=1E-8, debug=False):
-    """
-    Sets the integrator settings once
-    :param r: roadrunner instance
-    :return:
-    """
-    # absTol relative to the amounts
-    absolute_tolerance = absolute_tolerance * min(r.model.getCompartmentVolumes())
-    # set integrator settings
-    integrator = r.getIntegrator()
-    integrator.setValue('stiff', stiff)
-    integrator.setValue('absolute_tolerance', absolute_tolerance)
-    integrator.setValue('relative_tolerance', relative_tolerance)
-    integrator.setValue('variable_step_size', variable_step_size)
+            :returns tuple of simulation result and global parameters at end point of
+                    simulation (<NamedArray>, <DataFrame>)
+        """
+        # TODO: Tests:examples for parameters, initial concentrations and amounts.
+        # change parameters & recalculate initial assignments
+        if len(parameters) > 0:
+            concentrations = self.store_concentrations()
+            changed = self._set_parameters(parameters)
+            self.reset(SelectionRecord.INITIAL_GLOBAL_PARAMETER)
+            self.restore_concentrations(concentrations)
 
-    if debug:
-        print('-'*80)
-        print(r)
-        print('-'*80)
+        # set changed concentrations
+        if len(initial_concentrations) > 0:
+            self._set_initial_concentrations(initial_concentrations)
+        # set changed amounts
+        if len(initial_amounts) > 0:
+            self._set_initial_amounts(initial_amounts)
 
-def simulate(r, start, end, steps=None,
-             parameters={}, init_concentrations={}, init_amounts={}, debug=True):
-    """ Perform RoadRunner simulation.
-        Sets parameter values given in parameters dictionary &
-        initial values provided in dictionaries.
-
-        TODO: update & check if all the dependencies are handled correctly.
-        TODO: this is very specific to the simulation scenario (especially the resets).
-            So how to implement a more general simulation wropper with more options.
-        TODO: implement tests.
-        TODO: keep lean for fast simulations.
-        TODO: how to handle various simulation scenarios
-        TODO: provide examples for parameters, initial concentrations and amounts.
-
-        Necessary to handle the resets outside of roadrunner.
-        r.reset()  # only resets initial concentrations
-        r.reset(SelectionRecord.ALL)  # ?
-        r.reset(SelectionRecord.INITIAL_GLOBAL_PARAMETER)  # recalculates all initial assignments (but also concentrations)
-
-        Returns simulation results and global parameters at end of simulation.
-    """
-    # change parameters & recalculate initial assignments
-    cdict = store_concentrations(r)  # concentration backup
-    changed = _set_parameters(r, parameters)
-    r.reset(SelectionRecord.INITIAL_GLOBAL_PARAMETER)
-    restore_concentrations(r, cdict)  # restore concentrations
-    
-    # set changed concentrations
-    _set_initial_concentrations(r, init_concentrations)
-    # set changed amounts
-    _set_initial_amounts(r, init_amounts)
-
-    # integrate
-    timer_start = time.time()
-    if steps:
-        if r.getIntegrator().getValue('variable_step_size'):
+        if ("steps" in kwargs) and (self.getIntegrator().getValue('variable_step_size') == True):
             warnings.warn("steps provided in variable_step_size simulation !")
-        s = r.simulate(start=start, end=end, steps=steps)
-    else:
-        s = r.simulate(start=start, end=end)
-    timer_total = time.time() - timer_start
-    
 
-    # reset parameter changes    
-    # _set_parameters(r, changed)
-    # r.reset(SelectionRecord.INITIAL_GLOBAL_PARAMETER)
-    
-    # reset initial concentrations
-    # r.reset()
-    
-    if debug:
-        print('Integration time: {}'.format(timer_total))
+        # simulate (dangerous as long as simulate arguments not validated)
+        s = self.simulate(kwargs)
 
-    # simulation timecourse & global parameters for analysis
-    return s, global_parameters_dataframe(r)
+        if reset_parameters:
+            self._set_parameters(changed)
+            self.reset(SelectionRecord.INITIAL_GLOBAL_PARAMETER)
+
+        # simulation timecourse & global parameters for analysis
+        return s, self.global_parameters_dataframe()
 
 
-def store_concentrations(r):
-    """ Store FloatingSpecies concentrations of current model state. """
-    c = dict()
-    for sid in r.model.getFloatingSpeciesIds():
-        c[sid] = r["[{}]".format(sid)]
-    return c
+    def store_concentrations(self):
+        """ Store FloatingSpecies concentrations of current model state. """
+        return {"[{}]".format(sid): self["[{}]".format(sid)]
+                for sid in self.model.getFloatingSpeciesIds()}
+
+    def restore_concentrations(self, concentrations):
+        """ Restore the FloatingSpecies concentrations given in dictionary. """
+        assert(isinstance(concentrations, dict))
+        for key, value in concentrations.iteritems():
+            self[key] = value
+
+    def store_amounts(self):
+        """ Store FloatingSpecies amounts of current model state. """
+        # TODO: test
+        return {sid: self[sid] for sid in self.model.getFloatingSpeciesIds()}
+
+    def restore_amounts(self, amounts):
+        """ Restore FloatingSpecies amounts given in dictionary. """
+        assert(isinstance(amounts, dict))
+        for key, value in amounts.iteritems():
+            amounts.model['[{}]'.format(key)] = value
+
+    def _set_parameters(self, parameters):
+        """ Set given dictionary of parameters in model.
+            Returns dictionary of changes. """
+        changed = dict()
+        for key, value in parameters.iteritems():
+            changed[key] = self.model[key]
+            self.model[key] = value
+        return changed
+
+    def _set_initial_concentrations(self, init_concentrations):
+        """ Set initial concentrations from dictionary.
+            Returns dictionary of changes.
+        """
+        changed = dict()
+        for key, value in init_concentrations.iteritems():
+            changed[key] = self[key]
+            name = 'init([{}])'.format(key)
+            self[name] = value
+        return changed
+
+    def _set_initial_amounts(self, init_amounts):
+        """ Set initial values from dictionary.
+            Returns dictionary of changes.
+        """
+        changed = dict()
+        for key, value in init_amounts.iteritems():
+            changed[key] = self[key]
+            name = 'init({})'.format(key)
+            self.model[name] = value
+        return changed
 
 
-def restore_concentrations(r, cdict):
-    """ Restore the FloatingSpecies concentrations given in the dict. """
-    for key, value in cdict.iteritems():
-        r.model['[{}]'.format(key)] = value
+    #########################################################################
+    # Helper for units & selections
+    #########################################################################
+    def get_global_constant_parameters(self):
+        """ Subset of global parameters which are constant.
+            Set of parameter which has constant values and is not calculated
+            based on an initialAssignment.
+        """
+        # All global parameters which are constant have to be varied.
+        parameter_ids = self.model.getGlobalParameterIds()
+        import libsbml
+        doc = libsbml.readSBMLFromString(self.getSBML())
+        model = doc.getModel()
+        const_parameter_ids = []
+        for pid in parameter_ids:
+            p = model.getParameter(pid)
+            if p.constant:
+                const_parameter_ids.append(pid)
+        return const_parameter_ids
 
 
-def _set_parameters(r, parameters):
-    """ Set given dictionary of parameters in model.
-        Returns dictionary of changes. """
-    changed = dict()
-    for key, value in parameters.iteritems():
-        changed[key] = r.model[key]
-        r.model[key] = value 
-    return changed
+    #########################################################################
+    # DataFrames
+    #########################################################################
+
+    def global_parameters_dataframe(self):
+        """ Create GlobalParameter DataFrame. """
+        return DataFrame({'value': self.model.getGlobalParameterValues()},
+                         index=self.model.getGlobalParameterIds())
 
 
-def _set_initial_concentrations(r, init_concentrations):
-    """ Set initial concentrations from dictionary.
-        Returns dictionary of changes.
-    """
-    changed = dict()
-    for key, value in init_concentrations.iteritems():
-        changed[key] = r[key]
-        name = 'init([{}])'.format(key)
-        r[name] = value
-    return changed
+    def floating_species_dataframe(self):
+        """ Create FloatingSpecies DataFrame. """
+        return DataFrame({'concentration': self.model.getFloatingSpeciesConcentrations(),
+                          'amount': self.model.getFloatingSpeciesAmounts()},
+                         index=self.model.getFloatingSpeciesIds())
 
 
-def _set_initial_amounts(r, init_amounts):
-    """ Set initial values from dictionary.
-        Returns dictionary of changes.
-    """
-    changed = dict()
-    for key, value in init_amounts.iteritems():
-        changed[key] = r[key]
-        name = 'init({})'.format(key)
-        r.model[name] = value
-    return changed
+    def boundary_species_dataframe(self):
+        """ Create BoundingSpecies DataFrame. """
+        return DataFrame({'concentration': self.model.getBoundarySpeciesConcentrations(),
+                          'amount': self.model.getBoundarySpeciesAmounts()},
+                         index=self.model.getBoundarySpeciesIds())
 
+    #########################################################################
+    # Plotting
+    #########################################################################
+    @classmethod
+    def plot_results(results, *args, **kwargs):
+        """
+        :param results: list of result matrices
+        :return:
+        """
+        import matplotlib.pylab as plt
 
-#########################################################################
-# Plotting
-#########################################################################
-def plot_results(results, *args, **kwargs):
-    """
-    :param results: list of result matrices
-    :return:
-    """
-    import matplotlib.pylab as plt
-
-    plt.figure(figsize=(7, 4))
-    for s in results:
-        plt.plot(s[:, 0], s[:, 1:], *args, **kwargs)
-        # print('tend:', s[-1, 0])
-    # labels
-    plt_fontsize = 30
-    plt.xlabel('time [s]', fontsize=plt_fontsize)
-    plt.ylabel('species [mM]', fontsize=plt_fontsize)
+        plt.figure(figsize=(7, 4))
+        for s in results:
+            plt.plot(s[:, 0], s[:, 1:], *args, **kwargs)
+            # print('tend:', s[-1, 0])
+        # labels
+        plt_fontsize = 30
+        plt.xlabel('time [s]', fontsize=plt_fontsize)
+        plt.ylabel('species [mM]', fontsize=plt_fontsize)
