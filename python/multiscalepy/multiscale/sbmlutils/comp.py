69a08218c54e0e9532e4d139ff0b34ebebd5d7d3
Matthias Koenig <konigmatt@googlemail.com>
2016-03-31 12:26:35 +0200

    improving replacements in comp

--- python/multiscalepy/multiscale/sbmlutils/comp.py
+++ python/multiscalepy/multiscale/sbmlutils/comp.py
@@ -1,6 +1,12 @@
 """
 Utils for the creation and work with comp models.
 """
+from __future__ import print_function, division
+
+from mpmath.functions.factorials import fac2
+
+import factory
+
 # TODO: allow generic arguments the factory function and use them to set
 #   metaId, sbo, name, id,
 
@@ -31,7 +37,6 @@ def add_submodel_from_emd(mplugin, submodel_sid, emd):
 
     # ! gets the model belonging the SBASe !
     # model = emd.getModel()
-    import libsbml
     model = emd.getReferencedModel()
     if model.isSetSBOTerm():
         submodel.setSBOTerm(model.getSBOTerm())
@@ -82,7 +87,11 @@ def _create_port(model, pid, name=None, portRef=None, idRef=None, unitRef=None, 
     if idRef is not None:
         p.setIdRef(idRef)
     if unitRef is not None:
-        p.setUnitRef(unitRef)
+        unit_str = factory.get_unit_string(unitRef)
+        print("set unitRef")
+        print(unit_str)
+        res = p.setUnitRef(unit_str)
+        print(res)
     if metaIdRef is not None:
         p.setMetaIdRef(metaIdRef)
     if portType == PORT_TYPE_PORT:
@@ -101,30 +110,16 @@ def _create_port(model, pid, name=None, portRef=None, idRef=None, unitRef=None, 
 ##########################################################################
 # Replacement helpers
 ##########################################################################
-# TODO: more generic
+def replace_elements(model, sid, replaced_elements):
+    """
+    Replace elements in comp.
+    """
+    e = model.getElementBySId(sid)
+    eplugin = e.getPlugin("comp")
 
-def replace_compartment(model, cid, replaced_elements):
-    c = model.getCompartment(cid)
-    cplugin = c.getPlugin("comp")
-    for element in replaced_elements:
-        replaced_element = cplugin.createReplacedElement()
-        replaced_element.setSubmodelRef(element)
-        replaced_element.setIdRef(cid)
-
-
-def replace_species(model, sid, replaced_elements):
-    c = model.getSpecies(sid)
-    cplugin = c.getPlugin("comp")
-    for element in replaced_elements:
-        replaced_element = cplugin.createReplacedElement()
-        replaced_element.setSubmodelRef(element)
-        replaced_element.setIdRef(sid)
-
-
-def replace_parameters(model, sid, replaced_elements):
-    c = model.getParameter(sid)
-    cplugin = c.getPlugin("comp")
-    for element in replaced_elements:
-        replaced_element = cplugin.createReplacedElement()
-        replaced_element.setSubmodelRef(element)
-        replaced_element.setIdRef(sid)
+    for mid, rep_ids in replaced_elements.iteritems():
+        for rep_id in rep_ids:
+            print(sid, '--rep-->', mid, ':', rep_id)
+            replaced_element = eplugin.createReplacedElement()
+            replaced_element.setSubmodelRef(mid)
+            replaced_element.setIdRef(rep_id)
