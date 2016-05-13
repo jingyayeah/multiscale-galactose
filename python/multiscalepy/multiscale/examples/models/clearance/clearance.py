882fb6b72b1f5c73b3ba7de1013853b5c2fbcf45
Matthias Koenig <konigmatt@googlemail.com>
2016-05-13 16:26:35 +0200

    clearance model created

--- python/multiscalepy/multiscale/examples/models/clearance/clearance.py
+++ python/multiscalepy/multiscale/examples/models/clearance/clearance.py
@@ -4,22 +4,20 @@ Simple test model for the clearance of hepatic substances.
 
 from __future__ import print_function, division
 import os
-import Cell
-
 from sbmlutils.modelcreator import modelcreator
+import clearance_cell
 
 name = 'clearance'
-model_module = 'multiscale.examples.models'
 base_dir = os.path.dirname(os.path.abspath(__file__))
-sbml_path = os.path.join(base_dir, 'results', '{}_{}.xml'.format(Cell.mid, Cell.version))
+target_dir = os.path.join(base_dir, 'results')
+sbml_path = os.path.join(target_dir, '{}_{}.xml'.format(clearance_cell.mid, clearance_cell.version))
 
 
 def create_model():
     """ Create model. """
-    return modelcreator.create_model(target_dir=os.path.join(base_dir, 'results'),
-                                     model_info=['{}.{}'.format(model_module, 'hepatocyte'),
-                                                 '{}.{}'.format(model_module, name)],
-                                     f_annotations=None)
+    return modelcreator.create_model(modules=['multiscale.examples.models.templates.hepatocyte',
+                                              'multiscale.examples.models.clearance.clearance_cell'],
+                                     target_dir=target_dir)
 
 
 #################################################################################################
---
+++ python/multiscalepy/multiscale/examples/models/clearance/clearance_cell.py
@@ -0,0 +1,48 @@
+# -*- coding=utf-8 -*-
+"""
+Simple hepatic clearance model in SBML.
+
+The model is used to test the coupling to PKPD and the sinusoidal models.
+"""
+
+from libsbml import XMLNode
+from sbmlutils.modelcreator import templates
+import sbmlutils.modelcreator.modelcreator as mc
+
+##############################################################
+mid = 'Hepatic_clearance'
+version = 1
+notes = XMLNode.convertStringToXMLNode("""
+    <body xmlns='http://www.w3.org/1999/xhtml'>
+    <h1>Hepatic clearance</h1>
+    <h2>Description</h2>
+    <p>
+        This is a metabolism model of clearance metabolism in <a href="http://sbml.org">SBML</a> format.
+    </p>
+    """ + templates.terms_of_use + """
+    </body>
+    """)
+
+
+##############################################################
+# Species
+##############################################################
+species = [
+    # id : ('compartment', 'value', 'unit', 'boundaryCondition')
+    mc.Species(sid='S_ext', value=0.0021, unit='mM', compartment='e', name='substance S'),
+    mc.Species(sid='S', value=0.0, unit='mM', compartment='c', name='substance S'),
+    mc.Species(sid='P', value=0.0, unit='mM', compartment='c', name='product P'),
+]
+
+##############################################################
+# Rules
+##############################################################
+rules = [
+    mc.Rule(sid='f_cl', value='f_met * Vol_h', unit='-')
+]
+
+##############################################################
+# Reactions
+##############################################################
+import clearance_reactions as cr
+reactions = []
